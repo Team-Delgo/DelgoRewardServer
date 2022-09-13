0135cd0f11f99719ff6bc726de1ecbd6b7027a1c
Gupuroom <cjsrnr1114@gmail.com>
2022-09-13 11:14:54 +0900

    init commit

---
+++ src/main/java/com/delgo/reward/DelgoRewardServerApplication.java
@@ -0,0 +1,13 @@
+package com.delgo.reward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelgoRewardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelgoRewardServerApplication.class, args);
+    }
+
+}
