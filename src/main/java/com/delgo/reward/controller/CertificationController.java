0af674268b995d402ecf922a7fa5525fcdbb2acb
Gupuroom <cjsrnr1114@gmail.com>
2022-09-19 10:38:06 +0900

    /certification/register: 인증 조건 추가 -  6시간 이내 같은 장소 인증 불가능 ( 멍플만 ) - 하루에 같은 카테고리 5번 이상 인증 불가능

--- src/main/java/com/delgo/reward/controller/CertificationController.java
+++ src/main/java/com/delgo/reward/controller/CertificationController.java
@@ -36,25 +36,35 @@ public class CertificationController extends CommController {
     /*
      * 인증 등록
      * Request Data : CertificationDTO
-     * - TODO : 중복인증 막아야 함. ( 멍플은 가능해 보임 , 일반 인증은 ? )
      * Response Data : 등록한 인증 데이터 반환
      */
     @PostMapping("/register")
     public ResponseEntity register(@Validated @RequestBody CertificationDTO certificationDTO) {
-        // GeoCode 조회
-        Location userLocation = reverseGeoService.getReverseGeoData(new Location(certificationDTO.getLatitude(), certificationDTO.getLongitude()));
-        Code code = codeService.getGeoCodeBySIGUGUN(userLocation.getSIGUGUN()); // GeoCode
+        // 하루에 같은 카테고리 5번 이상 인증 불가능
+        if (!certificationService.checkCertRegister(certificationDTO.getUserId(), certificationDTO.getCategoryCode()))
+            return ErrorReturn(ApiCode.PARAM_ERROR); // TODO: 에러 코드 생성 필요
 
-        // 멍플 인증
+        // userId != 0이면 멍플 인증이라 판단
         if (certificationDTO.getMungpleId() != 0) {
+            // 6시간 이내 같은 장소 인증 불가능 (멍플만)
+            if (!certificationService.checkMungpleCertRegister(certificationDTO.getUserId(), certificationDTO.getMungpleId()))
+                return ErrorReturn(ApiCode.PARAM_ERROR); // TODO: 에러 코드 생성 필요
+
             Mungple mungple = mungpleService.getMungpleByMungpleId(certificationDTO.getMungpleId());
-            // 사용자 실수 방시 ( 멍플 인증일 경우 멍플의 카테고리 넣어준다.
+
+            // 사용자 실수 방지
             certificationDTO.setCategoryCode(mungple.getCategoryCode());
+            certificationDTO.setPlaceName(mungple.getPlaceName());
+
             // 멍플 <-> 유저와의 거리
-            double distance = geoService.getDistance(mungple.getRoadAddress(), certificationDTO.getLatitude(), certificationDTO.getLongitude());
+            double distance = geoService.getDistance(mungple.getRoadAddress(), certificationDTO.getLongitude(), certificationDTO.getLatitude());
             if (distance > 30) // 30m 이상 떨어진 곳에서 인증시 인증 불가
                 return ErrorReturn(ApiCode.TOO_FAR_DISTANCE);
         }
+
+        // GeoCode 조회
+        Location userLocation = reverseGeoService.getReverseGeoData(new Location(certificationDTO.getLatitude(), certificationDTO.getLongitude()));
+        Code code = codeService.getGeoCodeBySIGUGUN(userLocation.getSIGUGUN()); // GeoCode
 
         Certification certification = certificationService.registerCertification(certificationDTO.makeCertification(code));
         return SuccessReturn(certification);
