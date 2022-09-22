7537abeedbee6178570fd11e981224e00b7a783b
danpung2 <danpung_2@naver.com>
2022-09-22 15:36:59 +0900

    카테고리 기준 지역별 랭킹 매기기 - 수정중

--- src/main/java/com/delgo/reward/repository/JDBCTemplateRankingRepository.java
+++ src/main/java/com/delgo/reward/repository/JDBCTemplateRankingRepository.java
@@ -14,13 +14,41 @@ public class JDBCTemplateRankingRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
-    public List<Ranking> findRankingByPoint() {
-        return jdbcTemplate.query("select user_id, geo_code, weekly_point, RANK() over (partition by geo_code order by weekly_point desc) ranking from user;", rankingRowMapper());
+
+    public void insertRanking(Ranking ranking){
+        String sql = "insert into ranking values(?,?,?,?)";
+        jdbcTemplate.update(sql, ranking.getGeoCode(), ranking.getCategoryCode(), ranking.getRanking(), ranking.getUserId());
     }
 
-    private RowMapper<Ranking> rankingRowMapper() {
+    public List<Ranking> findRankingByPoint() {
+        return jdbcTemplate.query("select user_id, geo_code, weekly_point, RANK() over (partition by geo_code order by weekly_point desc) ranking from user;", rankingByPointRowMapper());
+    }
+
+    public List<Ranking> findRankingByCategory(String categoryCode) {
+        return jdbcTemplate.query("select category_code, user_id, geo_code, RANK() over (partition by category_code order by user_id desc) ranking from (select category_code, user_id, geo_code, count(*) from certification where category_code = \"" + categoryCode + "\" group by user_id) by_category_code;", rankingByCategoryRowMapper(categoryCode));
+    }
+
+    public List<Ranking> selectRanking(int userId, String categoryCode){
+        return jdbcTemplate.query("select * from ranking where user_id = " + userId + " and category_code = \"" + categoryCode + "\";", userByCategoryRowMapper());
+    }
+
+    private RowMapper<Ranking> userByCategoryRowMapper(){
         return (rs, rowNum) -> {
             Ranking ranking = Ranking.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).categoryCode("CA0000").build();
+            return ranking;
+        };
+    }
+
+    private RowMapper<Ranking> rankingByPointRowMapper() {
+        return (rs, rowNum) -> {
+            Ranking ranking = Ranking.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).categoryCode("CA0000").build();
+            return ranking;
+        };
+    }
+
+    private RowMapper<Ranking> rankingByCategoryRowMapper(String categoryCode) {
+        return (rs, rowNum) -> {
+            Ranking ranking = Ranking.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).categoryCode(categoryCode).build();
             return ranking;
         };
     }
