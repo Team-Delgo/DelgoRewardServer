a50bb0a877616afe41914f216a41bac28404fc7d
danpung2 <danpung_2@naver.com>
2022-09-22 19:43:00 +0900

    1. 포인트 / 카테고리 별 랭킹 테이블 구분 2. 포인트 별 랭킹 매기기 3. 카테고리 별 랭킹 매기기 - 수정중

--- src/main/java/com/delgo/reward/repository/JDBCTemplateRankingRepository.java
+++ src/main/java/com/delgo/reward/repository/JDBCTemplateRankingRepository.java
@@ -1,6 +1,7 @@
 package com.delgo.reward.repository;
 
-import com.delgo.reward.domain.Ranking;
+import com.delgo.reward.domain.ranking.RankingCategory;
+import com.delgo.reward.domain.ranking.RankingPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -14,42 +15,27 @@ public class JDBCTemplateRankingRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
-
-    public void insertRanking(Ranking ranking){
-        String sql = "insert into ranking values(?,?,?,?)";
-        jdbcTemplate.update(sql, ranking.getGeoCode(), ranking.getCategoryCode(), ranking.getRanking(), ranking.getUserId());
-    }
-
-    public List<Ranking> findRankingByPoint() {
+    public List<RankingPoint> findRankingByPoint() {
         return jdbcTemplate.query("select user_id, geo_code, weekly_point, RANK() over (partition by geo_code order by weekly_point desc) ranking from user;", rankingByPointRowMapper());
     }
 
-    public List<Ranking> findRankingByCategory(String categoryCode) {
+    public List<RankingCategory> findRankingByCategory(String categoryCode) {
+        System.out.println("select category_code, user_id, geo_code, RANK() over (partition by category_code order by user_id desc) ranking from (select category_code, user_id, geo_code, count(*) from certification where category_code = \"" + categoryCode + "\" group by user_id) by_category_code;");
         return jdbcTemplate.query("select category_code, user_id, geo_code, RANK() over (partition by category_code order by user_id desc) ranking from (select category_code, user_id, geo_code, count(*) from certification where category_code = \"" + categoryCode + "\" group by user_id) by_category_code;", rankingByCategoryRowMapper(categoryCode));
     }
 
-    public List<Ranking> selectRanking(int userId, String categoryCode){
-        return jdbcTemplate.query("select * from ranking where user_id = " + userId + " and category_code = \"" + categoryCode + "\";", userByCategoryRowMapper());
-    }
 
-    private RowMapper<Ranking> userByCategoryRowMapper(){
+    private RowMapper<RankingPoint> rankingByPointRowMapper() {
         return (rs, rowNum) -> {
-            Ranking ranking = Ranking.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).categoryCode("CA0000").build();
-            return ranking;
+            RankingPoint rankingPoint = RankingPoint.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).build();
+            return rankingPoint;
         };
     }
 
-    private RowMapper<Ranking> rankingByPointRowMapper() {
+    private RowMapper<RankingCategory> rankingByCategoryRowMapper(String categoryCode) {
         return (rs, rowNum) -> {
-            Ranking ranking = Ranking.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).categoryCode("CA0000").build();
-            return ranking;
-        };
-    }
-
-    private RowMapper<Ranking> rankingByCategoryRowMapper(String categoryCode) {
-        return (rs, rowNum) -> {
-            Ranking ranking = Ranking.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).categoryCode(categoryCode).build();
-            return ranking;
+            RankingCategory rankingCategory = RankingCategory.builder().userId(rs.getInt("user_id")).ranking(rs.getInt("ranking")).geoCode(rs.getString("geo_code")).categoryCode(categoryCode).build();
+            return rankingCategory;
         };
     }
 }
