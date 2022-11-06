5b8e5a96569e562f231e35148f8e042fdf1606b2
gupuroom <cjsrnr1114@naver.com>
2022-11-06 18:15:50 +0900

    WardOffice 수정 ( 아직 미완성 )

--- src/main/java/com/delgo/reward/repository/WardOfficeRepository.java
+++ src/main/java/com/delgo/reward/repository/WardOfficeRepository.java
@@ -5,5 +5,9 @@ import com.delgo.reward.domain.WardOffice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface WardOfficeRepository extends JpaRepository<WardOffice, Integer>, JpaSpecificationExecutor<WardOffice> {
+
+    Optional<WardOffice> findByGeoCode(String geoCode);
 }
