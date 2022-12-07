e78d68ed321f4031ccf3d73155f70ebd3ec23ef9
gupuroom <cjsrnr1114@naver.com>
2022-12-07 11:56:57 +0900

    Achievements 수정 - 업적 등록 api 추가 - cleancode

--- src/main/java/com/delgo/reward/controller/AchievementsController.java
+++ src/main/java/com/delgo/reward/controller/AchievementsController.java
@@ -1,9 +1,12 @@
 package com.delgo.reward.controller;
 
 import com.delgo.reward.comm.CommController;
-import com.delgo.reward.domain.Achievements;
-import com.delgo.reward.domain.Archive;
-import com.delgo.reward.dto.MainAchievementsDTO;
+import com.delgo.reward.domain.achievements.Achievements;
+import com.delgo.reward.domain.achievements.AchievementsCondition;
+import com.delgo.reward.domain.achievements.Archive;
+import com.delgo.reward.dto.achievements.AchievementsDTO;
+import com.delgo.reward.dto.achievements.MainAchievementsDTO;
+import com.delgo.reward.service.AchievementsConditionService;
 import com.delgo.reward.service.AchievementsService;
 import com.delgo.reward.service.ArchiveService;
 import lombok.RequiredArgsConstructor;
@@ -25,16 +28,50 @@ public class AchievementsController extends CommController {
 
     private final ArchiveService archiveService;
     private final AchievementsService achievementsService;
+    private final AchievementsConditionService achievementsConditionService;
+
+    /*
+     * 업적 등록
+     * Request Data :
+     * Response Data :
+     */
+    @PostMapping
+    public ResponseEntity register(@RequestBody AchievementsDTO dto) {
+
+        String img;
+        switch (dto.getCategoryCode()){
+            case "CA0001": img = "https://kr.object.ncloudstorage.com/reward-achivements/%ED%98%95%ED%83%9C%3D%EC%B9%B4%EB%93%9C%2C%20%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%3D%EC%82%B0%EC%B1%85.png";break;
+            case "CA0002": img = "https://kr.object.ncloudstorage.com/reward-achivements/%ED%98%95%ED%83%9C%3D%EC%B9%B4%EB%93%9C%2C%20%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%3D%EC%B9%B4%ED%8E%98.png";break;
+            case "CA0003": img = "https://kr.object.ncloudstorage.com/reward-achivements/%ED%98%95%ED%83%9C%3D%EC%B9%B4%EB%93%9C%2C%20%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%3D%EC%8B%9D%EB%8B%B9.png";break;
+            case "CA0004": img = "https://kr.object.ncloudstorage.com/reward-achivements/%ED%98%95%ED%83%9C%3D%EC%B9%B4%EB%93%9C%2C%20%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%3D%EB%AA%A9%EC%9A%95.png";break;
+            case "CA0005": img = "https://kr.object.ncloudstorage.com/reward-achivements/%ED%98%95%ED%83%9C%3D%EC%B9%B4%EB%93%9C%2C%20%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%3D%EB%AF%B8%EC%9A%A9.png";break;
+            case "CA0006": img = "https://kr.object.ncloudstorage.com/reward-achivements/%ED%98%95%ED%83%9C%3D%EC%B9%B4%EB%93%9C%2C%20%EC%B9%B4%ED%85%8C%EA%B3%A0%EB%A6%AC%3D%EB%B3%91%EC%9B%90.png";break;
+            default: img = ""; break;
+        }
+        Achievements achievements = achievementsService.register(dto.toEntity(img));
+        AchievementsCondition aCondition = achievementsConditionService.register(AchievementsCondition.builder()
+                .achievementsId(achievements.getAchievementsId())
+                .categoryCode(dto.getCategoryCode())
+                .count(dto.getCount())
+                .mungpleId(0)
+                .build()
+        );
+
+        log.info("achievements : {}", achievements);
+        log.info("aCondition : {}", aCondition);
+
+        return SuccessReturn();
+    }
 
     /*
      * 유저 획득 업적 리스트 조회
      * Request Data : userId
      * Response Data : 유저획득 업적 리스트
      */
-    @GetMapping("/user")
-    public ResponseEntity getUserData(@RequestParam Integer userId) {
+    @GetMapping("/user/{userId}")
+    public ResponseEntity getUserData(@PathVariable Integer userId) {
         List<Achievements> achievements = achievementsService.getAchievementsAll();
-        List<Archive> archives = archiveService.getArchiveByUserId(userId);
+        List<Archive> archives = archiveService.getArchiveByUserId(userId); // 유저 획득 업적
 
         for (Achievements achievement : achievements)
             for(Archive archive : archives)
@@ -42,6 +79,9 @@ public class AchievementsController extends CommController {
                     achievement.setIsActive(true);
                     achievement.setIsMain(archive.getIsMain());
                 }
+
+        String lockImg = "https://kr.object.ncloudstorage.com/reward-achivements/%EC%9E%A0%EA%B8%88%ED%99%94%EB%A9%B4.png";
+        achievements.stream().filter(a-> !a.getIsActive()).forEach(a -> a.setImgUrl(lockImg));
 
         List<Achievements> sortedMainAchievements = achievements.stream().filter(a->a.getIsMain()!=0).sorted(Comparator.comparing(Achievements::getIsMain)).collect(Collectors.toList());
         List<Achievements> notMainAchievements = achievements.stream().filter(a->a.getIsMain()==0).collect(Collectors.toList());
@@ -55,7 +95,7 @@ public class AchievementsController extends CommController {
      * Request Data : userId, archive
      * Response Data : 유저획득 업적 리스트
      */
-    @PostMapping("/main/set")
+    @PutMapping("/main")
     public ResponseEntity setMainAchievements(@RequestBody MainAchievementsDTO mainAchievementsDTO) {
         // 대표 업적 초기화
         archiveService.resetMainAchievements(mainAchievementsDTO.getUserId());
