42f950e728241814834d67e38a73d17a82503972
gupuroom <cjsrnr1114@naver.com>
2022-12-14 16:09:57 +0900

    수정 - 유저 차단 api 수정 : 차단한 유저 객체 반환 - 회원가입, 로그인 api 수정 : 견종 반환, 회원가입시 사진 수정.

--- src/main/java/com/delgo/reward/controller/BanListController.java
+++ src/main/java/com/delgo/reward/controller/BanListController.java
@@ -5,19 +5,20 @@ import com.delgo.reward.service.BanService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/ban")
 public class BanListController extends CommController {
     private final BanService banService;
 
-    @GetMapping("/ban")
-    public ResponseEntity<?> ban(@RequestParam Integer userId, @RequestParam Integer banUserId){
-        banService.ban(userId, banUserId);
-        return SuccessReturn();
+    @PostMapping("/{userId}/{banUserId}")
+    public ResponseEntity<?> ban(@PathVariable Integer userId, @PathVariable Integer banUserId){
+        return SuccessReturn(banService.ban(userId, banUserId));
     }
 }
