f1bdea76c29f94259434172d2fab3a2bf0d1227a
gupuroom <cjsrnr1114@naver.com>
2022-12-18 16:56:21 +0900

    업적 등록 API 수정 : 업적 등록 시 해당 업적의 조건 반환 추가

--- src/main/java/com/delgo/reward/domain/achievements/Achievements.java
+++ src/main/java/com/delgo/reward/domain/achievements/Achievements.java
@@ -1,16 +1,23 @@
 package com.delgo.reward.domain.achievements;
 
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Getter
 @Entity
 @Builder
-@ToString
+@DynamicInsert
 @NoArgsConstructor
 @AllArgsConstructor
 public class Achievements {
@@ -22,18 +29,29 @@ public class Achievements {
     private String imgUrl; // 업적 아이콘 이미지 url (대표 업적 설정되었을 때만 사용 됨)
     private Boolean isMungple; // 업적 조건에 멍플 조건이 있는지 여부 체크
 
+    @CreationTimestamp
+    private LocalDateTime registDt; // 등록 날짜
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "achievements", cascade = CascadeType.REMOVE)
+    private List<AchievementsCondition> achievementsCondition;
+
     @Transient
     @Builder.Default
     private Integer isMain = 0; // 대표 이미지일 경우 순서 표시
     @Transient
     @Builder.Default
     private Boolean isActive = false;
-
-    @CreationTimestamp
-    private LocalDateTime registDt; // 등록 날짜
+    @Transient
+    @Builder.Default
+    private Boolean conditionCheck = true;
 
     public void beActive(int order) {
         this.isActive = true;
         this.isMain = order;
+    }
+
+    public void setConditionCheck(boolean check) {
+        this.conditionCheck = check;
     }
 }
