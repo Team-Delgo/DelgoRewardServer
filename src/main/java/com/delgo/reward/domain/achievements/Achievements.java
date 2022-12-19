7cac40a86ba1c87790b7d4a5e0ee25d136115433
Gupuroom <cjsrnr1114@gmail.com>
2022-12-19 10:21:35 +0900

    업적 API 수정 - checkEarnAchievements 수정 : Query로 코드 단축

--- src/main/java/com/delgo/reward/domain/achievements/Achievements.java
+++ src/main/java/com/delgo/reward/domain/achievements/Achievements.java
@@ -1,11 +1,8 @@
 package com.delgo.reward.domain.achievements;
 
-
+import lombok.*;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 
