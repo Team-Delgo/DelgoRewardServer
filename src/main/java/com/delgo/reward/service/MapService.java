05c36f716f3ebf023a18ee822ad61e4aa8ba2f0a
Gupuroom <cjsrnr1114@gmail.com>
2023-01-03 16:13:07 +0900

    Map API 추가 - delgo-map에서 사용할 /map/mungple 추가 - Mungple[domain]에서 detailUrl 추가

--- src/main/java/com/delgo/reward/service/MapService.java
+++ src/main/java/com/delgo/reward/service/MapService.java
@@ -1,13 +1,14 @@
 package com.delgo.reward.service;
 
 
+import com.delgo.reward.comm.code.CategoryCode;
+import com.delgo.reward.domain.Mungple;
 import com.delgo.reward.domain.certification.Certification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -28,12 +29,14 @@ public class MapService {
         List<Certification> certifications = certService.getLive(userId);  // 라이브 인증 리스트 조회
         certifications.forEach(c ->c.liked(likeListService.hasLiked(userId, c.getCertificationId()))); // 유저가 좋아요 누른 인증 체크
 
-        HashMap<String, Object> resultMap = new HashMap<>();
-        resultMap.put("mungpleList", mungpleService.getMungpleAll()); // mungpleList :  멍플 리스트
-        resultMap.put("wardOffice", wardOfficeService.getWardOfficeByGeoCode(userService.getUserById(userId).getGeoCode())); // wardOfficeList : 구군청 위치
-        resultMap.put("certNormalList", certifications.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()));  // certNormalList : 일반 인증 리스트 ( 하얀 테두리 )
-        resultMap.put("certMungpleList", certifications.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList())); // certMunpleList : 멍플 인증 리스트 ( 주황 테두리 )
+       return Map.of(
+                "mungpleList", mungpleService.getMungpleAll(), // mungpleList :  멍플 리스트
+                "wardOffice", wardOfficeService.getWardOfficeByGeoCode(userService.getUserById(userId).getGeoCode()), // wardOfficeList : 구군청 위치
+                "certNormalList", certifications.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()), // certNormalList : 일반 인증 리스트 ( 하얀 테두리 )
+                "certMungpleList", certifications.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList())); // certMunpleList : 멍플 인증 리스트 ( 주황 테두리 )
+    }
 
-        return resultMap;
+    public List<Mungple> getMapOfMungple(CategoryCode categoryCode) {
+        return mungpleService.getMungpleByCategoryCode(categoryCode.getCode());
     }
 }
