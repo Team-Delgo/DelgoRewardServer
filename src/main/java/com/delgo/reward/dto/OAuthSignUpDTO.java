18cfda0a103efbe77600852611374d2d72c24ea5
danpung2 <danpung_2@naver.com>
2023-01-30 15:35:04 +0900

    CleanCode - User builder DTO로 이동

--- src/main/java/com/delgo/reward/dto/OAuthSignUpDTO.java
+++ src/main/java/com/delgo/reward/dto/OAuthSignUpDTO.java
@@ -1,5 +1,7 @@
 package com.delgo.reward.dto;
 
+import com.delgo.reward.domain.pet.Pet;
+import com.delgo.reward.domain.user.User;
 import com.delgo.reward.domain.user.UserSocial;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -37,4 +39,36 @@ public class OAuthSignUpDTO {
 
     // KAKAO, NAVER 때는 필요없어서 @NotNull 넣지 않음.
     private String appleUniqueNo;
+
+    public User makeUserSocial(UserSocial userSocial, String address){
+        return User.builder()
+                .name(userName)
+                .email(email)
+                .phoneNo(phoneNo.replaceAll("[^0-9]", ""))
+                .userSocial(userSocial)
+                .address(address)
+                .geoCode(geoCode)
+                .pGeoCode(pGeoCode)
+                .build();
+    }
+
+    public User makeUserApple(String appleUniqueNo, String address){
+        return User.builder()
+                .name(userName)
+                .email(email)
+                .phoneNo(phoneNo.replaceAll("[^0-9]", ""))
+                .userSocial(UserSocial.A)
+                .address(address)
+                .geoCode(geoCode)
+                .pGeoCode(pGeoCode)
+                .build();
+    }
+
+    public Pet makePet(){
+        return Pet.builder()
+                .name(petName)
+                .breed(breed)
+                .birthday(birthday)
+                .build();
+    }
 }
