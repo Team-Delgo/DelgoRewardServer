2fbc7d429bb8d8e375b214bc08c5d2f0ebe957ac
Gupuroom <cjsrnr1114@gmail.com>
2023-02-09 10:43:36 +0900

    Cert 수정 - Deprecated DTO, Service .. 삭제 - exposedCertList 추가 : 지도에서 사용자들에게 노출시킬 인증 리스트

--- src/main/java/com/delgo/reward/service/CertService.java
+++ src/main/java/com/delgo/reward/service/CertService.java
@@ -138,16 +138,6 @@ public class CertService {
         );
     }
 
-    // Live Certification 조회
-    public List<Certification> getLive(int userId) {
-        return certRepository.findByUserIdAndIsLive(userId, true);
-    }
-
-    // Past Certification 조회
-    public List<Certification> getPast(int userId) {
-        return certRepository.findByUserIdAndIsLive(userId, false);
-    }
-
     // Id로 Certification 조회
     public Certification getCert(int certificationId) {
         return certRepository.findById(certificationId)
@@ -159,10 +149,15 @@ public class CertService {
         return certRepository.findByUserId(userId);
     }
 
-    // 최근 2개 인증 조회
+    // 최근 인증 조회
     public List<Certification> getRecentCert(int userId, int count) {
         return certRepository.findRecentCert(userId, count).stream()
                 .peek(cert -> setUserAndLike(userId, cert)).collect(Collectors.toList());
+    }
+
+    // 노출 가능한 인증 조회
+    public List<Certification> getExposedCert(int count) {
+        return certRepository.findByIsExpose(count);
     }
 
     // 좋아요 Check
@@ -184,65 +179,4 @@ public class CertService {
     public void minusCommentCount(int certificationId) {
         jdbcTemplateRankingRepository.minusCommentCount(certificationId);
     }
-
-    // 하루에 같은 카테고리 5번 이상 인증 불가능 체크
-    public boolean checkCategoryCountIsFive(int userId, String categoryCode, boolean isLive) {
-        List<Certification> list = certRepository.findByUserIdAndCategoryCodeAndIsLiveAndRegistDtBetween(userId, categoryCode, isLive, start, end);
-        return list.size() < 5;
-    }
-
-    // 6시간 이내 같은 장소 인증 불가능 ( 멍플만 ) ( Deprecated )
-//    public boolean checkContinueRegist(int userId, int mungpleId, boolean isLive) {
-//        List<Certification> certifications =
-//                certRepository.findByUserIdAndMungpleIdAndIsLiveAndRegistDtBetween(userId, mungpleId, isLive, start,
-//                        end).stream().sorted(Comparator.comparing(Certification::getRegistDt).reversed()).collect(Collectors.toList());
-//        if (certifications.isEmpty())
-//            return true;
-//
-//        // 최근 등록시간이랑 비교시간이 6시간(21600초)이내일 경우 오류 반환
-//        return ChronoUnit.SECONDS.between(certifications.get(0).getRegistDt(), LocalDateTime.now()) > 21600;
-//    }
-
-    // Live 등록 ( Deprecated )
-//    public Certification registerLive(LiveCertDTO dto) {
-//        Location location = reverseGeoService.getReverseGeoData(new Location(dto.getLatitude(), dto.getLongitude()));
-//        Certification certification = save(dto.toEntity(location));
-//
-//        // 획득 가능한 업적 Check
-//        List<Achievements> earnAchievements = achievementsService.checkEarnedAchievements(dto.getUserId(), dto.getMungpleId() != 0);
-//        if (!earnAchievements.isEmpty()) {
-//            archiveService.registerArchives(earnAchievements.stream()
-//                    .map(achievement -> achievement.toArchive(dto.getUserId())).collect(Collectors.toList()));
-//            certification.setAchievements(earnAchievements);
-//        }
-//
-//        // 사진 파일 저장 추가
-//        String photoUrl = photoService.uploadCertEncodingFile(certification.getCertificationId(), dto.getPhoto());
-//        save(certification.setPhotoUrl(photoUrl));
-//        // Point 부여
-//        pointService.givePoint(userService.getUserById(dto.getUserId()).getUserId(), CategoryCode.valueOf(dto.getCategoryCode()).getPoint());
-//        // 랭킹 실시간으로 집계
-//        rankingService.rankingByPoint();
-//
-//        log.info("requestBody : {}", dto.toLog()); // log 출력 전 photo 삭제
-//        return certification;
-//    }
-
-    // Past 등록 ( Deprecated )
-//    public Certification registerPast(PastCertDTO dto) {
-//        boolean isMungple = (dto.getMungpleId() != 0);
-//        Certification certification = save(!isMungple ? dto.toEntity()
-//                : dto.toEntity(mungpleService.getMungpleById(dto.getMungpleId())));
-//
-//        // 획득 가능한 업적 Check
-//        List<Achievements> earnedAchievements = achievementsService.checkEarnedAchievements(dto.getUserId(), dto.getMungpleId() != 0);
-//        if (!earnedAchievements.isEmpty()) {
-//            archiveService.registerArchives(earnedAchievements.stream()
-//                    .map(achievement -> achievement.toArchive(dto.getUserId())).collect(Collectors.toList()));
-//            certification.setAchievements(earnedAchievements);
-//        }
-//
-//        return save(certification);
-//    }
-
 }
