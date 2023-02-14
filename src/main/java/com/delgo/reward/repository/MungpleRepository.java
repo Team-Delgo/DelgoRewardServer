794a25f65c1ecfa1cd9dfcf0812246a2ba3c32c5
Gupuroom <cjsrnr1114@gmail.com>
2023-02-14 10:50:58 +0900

    Mungple API 추가 - 가장 인증이 많은 Mungple 조회하는 api 추가

--- src/main/java/com/delgo/reward/repository/MungpleRepository.java
+++ src/main/java/com/delgo/reward/repository/MungpleRepository.java
@@ -4,6 +4,7 @@ package com.delgo.reward.repository;
 import com.delgo.reward.domain.Mungple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.util.List;
@@ -17,4 +18,7 @@ public interface MungpleRepository extends JpaRepository<Mungple, Integer>, JpaS
     List<Mungple> findByCategoryCodeAndIsActive(String categoryCode, boolean isActive);
 
     List<Mungple> findAllByIsActive(boolean isActive);
+
+    @Query(value = "select * from mungple where is_active = true and mungple_id in (select mungple_id from certification group by mungple_id order by count(*) desc) limit ?", nativeQuery = true)
+    List<Mungple> findMungpleOfMostCount(int count);
 }
