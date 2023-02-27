5d8bab6841bbb7c44e745c7933ce630081bd2e01
Gupuroom <cjsrnr1114@gmail.com>
2023-02-27 17:03:48 +0900

    Mungple 등록 API 수정 - 썸네일, 디테일페이지 같이 저장하게 수정

--- src/main/java/com/delgo/reward/controller/MungpleController.java
+++ src/main/java/com/delgo/reward/controller/MungpleController.java
@@ -4,15 +4,18 @@ import com.delgo.reward.comm.CommController;
 import com.delgo.reward.comm.exception.ApiCode;
 import com.delgo.reward.comm.code.CategoryCode;
 import com.delgo.reward.comm.ncp.GeoService;
+import com.delgo.reward.domain.Mungple;
 import com.delgo.reward.domain.common.Location;
 import com.delgo.reward.dto.MungpleDTO;
 import com.delgo.reward.service.MungpleService;
+import com.delgo.reward.service.PhotoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Slf4j
@@ -23,19 +26,28 @@ public class MungpleController extends CommController {
 
     private final GeoService geoService;
     private final MungpleService mungpleService;
+    private final PhotoService photoService;
 
     /*
-     * 멍플 조회
+     * 멍플 등록
      * Request Data : MungpleDTO
      * - 주소로 위도, 경도 구할 수 있어야 함.
      * Response Data : 등록한 멍플 데이터 반환
      */
-    @PostMapping
-    public ResponseEntity register(@Validated @RequestBody MungpleDTO dto) {
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity register(@Validated @RequestPart MungpleDTO dto, @RequestPart MultipartFile thumbnail, @RequestPart MultipartFile mungpleNote) {
+        if (thumbnail.isEmpty()) return ErrorReturn(ApiCode.PARAM_ERROR);
+        if (mungpleNote.isEmpty()) return ErrorReturn(ApiCode.PARAM_ERROR);
+
         Location location = geoService.getGeoData(dto.getAddress()); // 위도, 경도
-        return (!mungpleService.isMungpleExisting(location))
-                ? SuccessReturn(mungpleService.register(dto.toEntity(location)))
-                : ErrorReturn(ApiCode.MUNGPLE_DUPLICATE_ERROR);
+        if (!mungpleService.isMungpleExisting(location)) ErrorReturn(ApiCode.MUNGPLE_DUPLICATE_ERROR);
+
+        Mungple mungple = mungpleService.register(dto.toEntity(location));
+
+        photoService.uploadMungple(mungple.getMungpleId(), thumbnail);
+        photoService.uploadMungpleNote(mungple.getMungpleId(), mungpleNote);
+
+        return SuccessReturn(mungple);
     }
 
     /*
