7f4f693887645fa44c0c3bf3495da5c5ee244d84
Gupuroom <cjsrnr1114@gmail.com>
2023-02-28 11:34:50 +0900

    PhotoService 수정 - ENUM 사용해서 직관적으로 변경

--- src/main/java/com/delgo/reward/comm/ncp/storage/BucketName.java
+++ src/main/java/com/delgo/reward/comm/ncp/storage/BucketName.java
@@ -2,21 +2,25 @@ package com.delgo.reward.comm.ncp.storage;
 
 
 public enum BucketName {
-    ACHIEVEMENTS("reward-achievements","업적 사진"),
-    CERTIFICATION("reward-certification","인증 사진"), // 산책
-    MUNGPLE("reward-mungple","멍플 사진"), // 카페
-    MUNGPLE_NOTE("reward-mungplenote","멍플 노트"), // 식당
-    PROFILE("reward-profile","유저 프로필");
+    ACHIEVEMENTS("reward-achievements","https://kr.object.ncloudstorage.com/reward-achievements/"),
+    CERTIFICATION("reward-certification","https://kr.object.ncloudstorage.com/reward-certification/"), // 산책
+    MUNGPLE("reward-mungple", "https://kr.object.ncloudstorage.com/reward-mungple/"), // 카페
+    MUNGPLE_NOTE("reward-mungplenote", "https://kr.object.ncloudstorage.com/reward-mungplenote/"), // 식당
+    PROFILE("reward-profile","https://kr.object.ncloudstorage.com/reward-profile/");
 
     private final String name;
-    private final String des;
+    private final String url;
 
-    BucketName(String name, String des) {
+    BucketName(String name, String url) {
         this.name = name;
-        this.des = des;
+        this.url = url;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public String getUrl() {
+        return this.url;
     }
 }
