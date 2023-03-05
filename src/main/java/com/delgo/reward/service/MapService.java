313feacba1da3539116e7e13800c7d5b3abaea99
gupuroom <cjsrnr1114@naver.com>
2023-03-05 16:39:11 +0900

    Map api 수정 - exposedList 수정

--- src/main/java/com/delgo/reward/service/MapService.java
+++ src/main/java/com/delgo/reward/service/MapService.java
@@ -25,19 +25,19 @@ public class MapService {
 
     public Map<String, Object> getMap(int userId) {
         List<Certification> certifications = certService.getCertByUserId(userId);  // 인증 리스트 조회
-        certifications.forEach(c -> c.liked(likeListService.hasLiked(userId, c.getCertificationId()))); // 유저가 좋아요 누른
+        if (userId != 0) certifications.forEach(c -> c.liked(likeListService.hasLiked(userId, c.getCertificationId()))); // 유저가 좋아요 누른
 
         List<Certification> exposedCertList = certService.getExposedCert(3);  // 노출시킬 인증 리스트 조회
-        exposedCertList.forEach(c -> c.liked(likeListService.hasLiked(userId, c.getCertificationId())));
+        if (userId != 0) exposedCertList.forEach(c -> c.liked(likeListService.hasLiked(userId, c.getCertificationId())));
 
         return (userId == 0) ? Map.of("mungpleList", mungpleService.getMungpleAll()) // 멍플 리스트
                 : Map.of(
                 "mungpleList", mungpleService.getMungpleAll(), // 멍플 리스트
-                "wardOffice", wardOfficeService.getWardOfficeByGeoCode(userService.getUserById(userId).getGeoCode()),// 구군청 위치
+//                "wardOffice", wardOfficeService.getWardOfficeByGeoCode(userService.getUserById(userId).getGeoCode()),// 구군청 위치
                 "normalCertList", certifications.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()), // 일반 인증 리스트
                 "mungpleCertList", certifications.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList()), // 멍플 인증 리스트
                 "exposedNormalCertList", exposedCertList.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()), // 사용자들에게 노출시킬 인증 리스트
-                "exposedMungpleCertList", exposedCertList.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList())); // 사용자들에게 노출시킬 인증 리스트
+                "exposedMungpleCertList", exposedCertList.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList())); // 사용자들에게 노출시킬 인증 리스트
     }
 
     public Map<String, List<Certification>> test(){
