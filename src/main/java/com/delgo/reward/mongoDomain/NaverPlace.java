fa4f50fdc6172d143b9ac2f90d316d84c16963bf
Gupuroom <cjsrnr1114@naver.com>
2023-03-27 23:45:02 +0900

    NaverPlace MongoDB에 저장하는 부분 추가

---
+++ src/main/java/com/delgo/reward/mongoDomain/NaverPlace.java
@@ -0,0 +1,30 @@
+package com.delgo.reward.mongoDomain;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection="naver_place")
+public class NaverPlace {
+    @Id
+    private String id;
+    @Field("placeName")
+    private String placeName;
+    @Field("address")
+    private String address;
+    @Field("category")
+    private String category;
+    @Field("photos")
+    private List<String> photos;
+
+}
