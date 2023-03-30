ff4795c5e4a4e410c1d9e1de705906516581bc6a
Gupuroom <cjsrnr1114@gmail.com>
2023-03-30 15:35:18 +0900

    Calendar DTO -> Record로 변경

---
+++ src/main/java/com/delgo/reward/record/CalendarRecord.java
@@ -0,0 +1,8 @@
+package com.delgo.reward.record;
+
+import com.delgo.reward.domain.certification.Certification;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record CalendarRecord(LocalDate date, Boolean isAchievements, List<Certification> dateList) {}
