73d2662cc9c103cc5f78cd21835881c53eb10d39
Gupuroom <cjsrnr1114@gmail.com>
2023-03-30 15:51:15 +0900

    Calendar Record 수정

--- src/main/java/com/delgo/reward/record/CalendarRecord.java
+++
@@ -1,8 +0,0 @@
-package com.delgo.reward.record;
-
-import com.delgo.reward.domain.certification.Certification;
-
-import java.time.LocalDate;
-import java.util.List;
-
-public record CalendarRecord(LocalDate date, Boolean isAchievements, List<Certification> dateList) {}
