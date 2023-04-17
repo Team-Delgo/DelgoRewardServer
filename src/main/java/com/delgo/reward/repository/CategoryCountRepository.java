1b18c460e586fe82d1031681d0151a066ead3785
danpung2 <danpung_2@naver.com>
2023-04-17 16:16:34 +0900

    modify: 유저 생성/삭제 시 카테고리 카운트 생성/삭제

--- src/main/java/com/delgo/reward/repository/CategoryCountRepository.java
+++ src/main/java/com/delgo/reward/repository/CategoryCountRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface CategoryCountRepository extends JpaRepository<CategoryCount, Integer> {
     Optional<CategoryCount> findByUserId(int userId);
+    void deleteByUserId(int userId);
 }
