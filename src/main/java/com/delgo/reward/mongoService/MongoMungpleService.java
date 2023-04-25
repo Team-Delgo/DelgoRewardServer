af34f5b9caf7e8d5b2b9c0eae5aec55c1c83ce8d
Gupuroom <cjsrnr1114@gmail.com>
2023-04-25 14:55:33 +0900

    Mungple 위치 기반 조회 구현 - centerSphere 사용

--- src/main/java/com/delgo/reward/mongoService/MongoMungpleService.java
+++ src/main/java/com/delgo/reward/mongoService/MongoMungpleService.java
@@ -1,10 +1,16 @@
 package com.delgo.reward.mongoService;
 
 import com.delgo.reward.domain.Mungple;
+import com.delgo.reward.mongoDomain.MongoMungple;
 import com.delgo.reward.mongoRepository.MongoMungpleRepository;
 import com.delgo.reward.service.MungpleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,14 +19,26 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class MongoMungpleService {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     private final MongoMungpleRepository mongoMungpleRepository;
     private final MungpleService mungpleService;
 
-
-    public void makeMungplestoMongo() {
-        List<Mungple> mungples = mungpleService.getMungpleAll();
+    public void makeMungpletoMongo() {
+        List<Mungple> mungples = mungpleService.getMungpleByMap();
         for(Mungple m : mungples){
             mongoMungpleRepository.save(m.toMongo());
         }
     }
+
+    public List<MongoMungple> findWithin3Km(double latitude, double longitude) {
+        double maxDistanceInRadians = 2 / 6371.01;
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("location").withinSphere(new Circle(longitude, latitude, maxDistanceInRadians)));
+
+        return mongoTemplate.find(query, MongoMungple.class);
+    }
 }
+
