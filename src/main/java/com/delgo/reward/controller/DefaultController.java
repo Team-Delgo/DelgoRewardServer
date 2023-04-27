ed30528f24742dc4a3e83d3fbf51436eaeede00f
Gupuroom <cjsrnr1114@gmail.com>
2023-04-27 16:49:37 +0900

    GET Version API 추가

--- src/main/java/com/delgo/reward/controller/DefaultController.java
+++ src/main/java/com/delgo/reward/controller/DefaultController.java
@@ -1,31 +1,35 @@
 package com.delgo.reward.controller;
 
 import com.delgo.reward.comm.CommController;
-import com.delgo.reward.service.PhotoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping(value ="/api")
 public class DefaultController extends CommController {
 
-    private final PhotoService photoService;
+    @Value("${version}")
+    String version;
 
-    @RequestMapping(value ="/")
+    @RequestMapping
     public ResponseEntity<?> defaultResponse() {
         return SuccessReturn();
     }
 
-    @RequestMapping(value ="/test")
-    public ResponseEntity<?> test(@RequestParam String url) {
-                photoService.checkCorrectPhoto(url);
-
+    @RequestMapping(value ="/")
+    public ResponseEntity<?> defaultResponse2() {
         return SuccessReturn();
+    }
+
+    @GetMapping(value ="/version")
+    public ResponseEntity<?> getVersion() {
+        return SuccessReturn(version);
     }
 }
