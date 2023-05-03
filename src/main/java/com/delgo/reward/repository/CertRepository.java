52a25da9a57be3b3d80c4b9a7ca502ff10c3d1b2
Gupuroom <cjsrnr1114@gmail.com>
2023-05-04 08:21:16 +0900

    필요없는 WriteRecord 삭제 - Cert Refactoring 마무리

--- src/main/java/com/delgo/reward/repository/CertRepository.java
+++ src/main/java/com/delgo/reward/repository/CertRepository.java
@@ -36,9 +36,6 @@ public interface CertRepository extends JpaRepository<Certification, Integer>, J
     @Query(value = "select c from Certification c where c.user.userId = :userId and  c.createdDate between :startDt and :endDt order by c.createdDate desc")
     List<Certification> findCertByDateAndUser(@Param("userId") int userId, @Param("startDt") LocalDateTime startDt, @Param("endDt") LocalDateTime endDate);
 
-    @Query(value = "select c from Certification c where c.createdDate between :startDt and :endDt order by c.createdDate desc")
-    List<Certification> findCertByDate(@Param("startDt") LocalDateTime startDt, @Param("endDt") LocalDateTime endDate);
-
     @EntityGraph(attributePaths = {"user", "likeLists"})
     @Query(value = "select c from Certification c where c.user.userId  not in (select b.banUserId from BanList b where b.userId = :userId) and c.isCorrectPhoto = true")
     Slice<Certification> findAllByPaging(@Param("userId") int userId, Pageable pageable);
@@ -47,9 +44,6 @@ public interface CertRepository extends JpaRepository<Certification, Integer>, J
     @Query(value = "select c from Certification c where c.mungpleId = :mungpleId and c.isCorrectPhoto = true")
     Slice<Certification> findCertByMungple(@Param("mungpleId") int mungpleId, Pageable pageable);
 
-    @Query(value = "select count(c) from Certification c where c.user.userId = :userId and c.mungpleId != 0")
-    Integer countOfCertByMungpleAndUser(@Param("userId") int userId);
-
     @EntityGraph(attributePaths = {"user", "likeLists"})
     @Query(value = "select c from Certification c where c.user.userId not in (select b.banUserId from BanList b where b.userId = :userId) and c.isCorrectPhoto = true order by c.createdDate desc")
     List<Certification> findRecentCert(@Param("userId") int userId, Pageable pageable);
@@ -57,6 +51,13 @@ public interface CertRepository extends JpaRepository<Certification, Integer>, J
     @EntityGraph(attributePaths = {"user", "likeLists"})
     @Query(value = "SELECT c FROM Certification c where c.isExpose = true and c.isCorrectPhoto = true order by RAND()")
     List<Certification> findByIsExpose(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"user", "likeLists"})
+    @Query(value = "select c from Certification c where c.user.userId not in (select b.banUserId from BanList b where b.userId = :userId) and c.certificationId != :certificationId and c.isCorrectPhoto = true")
+    Slice<Certification> findAllExcludeSpecificCert(@Param("userId") int userId, @Param("certificationId") int certificationId, Pageable pageable);
+
+    @Query(value = "select count(c) from Certification c where c.user.userId = :userId and c.mungpleId != 0")
+    Integer countOfCertByMungpleAndUser(@Param("userId") int userId);
 
     @Query(value = "SELECT c FROM Certification c where c.pGeoCode = :pGeoCode order by RAND()")
     List<Certification> findByPGeoCode(@Param("pGeoCode") String pGeoCode, Pageable pageable);
@@ -67,10 +68,9 @@ public interface CertRepository extends JpaRepository<Certification, Integer>, J
     @Query(value = "SELECT c FROM Certification c where c.pGeoCode = :pGeoCode and not c.geoCode = :geoCode order by RAND()")
     List<Certification> findByPGeoCodeExceptGeoCode(@Param("pGeoCode") String pGeoCode, @Param("geoCode") String geoCode, Pageable pageable);
 
-    @EntityGraph(attributePaths = {"user", "likeLists"})
-    @Query(value = "select c from Certification c where c.user.userId not in (select b.banUserId from BanList b where b.userId = :userId) and c.certificationId != :certificationId and c.isCorrectPhoto = true")
-    Slice<Certification> findAllExcludeSpecificCert(@Param("userId") int userId, @Param("certificationId") int certificationId, Pageable pageable);
-
     @Query(value = "select count(c) from Certification c where c.user.userId = :userId and c.categoryCode = :categoryCode and c.mungpleId = :mungpleId")
     Integer countCertByCategory(@Param("userId") int userId, @Param("categoryCode") String categoryCode, @Param("mungpleId") int mungpleId);
+
+    @Query(value = "select c from Certification c where c.createdDate between :startDt and :endDt order by c.createdDate desc")
+    List<Certification> findCertByDate(@Param("startDt") LocalDateTime startDt, @Param("endDt") LocalDateTime endDate);
 }
