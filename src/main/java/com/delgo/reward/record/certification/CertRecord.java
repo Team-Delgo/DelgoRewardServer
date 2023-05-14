91cdd1c594c55901f8a127fa6438a73199602d23
danpung2 <danpung_2@naver.com>
2023-05-14 15:20:17 +0900

    modify: @NotBlank -> @NotNull

--- src/main/java/com/delgo/reward/record/certification/CertRecord.java
+++ src/main/java/com/delgo/reward/record/certification/CertRecord.java
@@ -12,7 +12,7 @@ public record CertRecord(
         @NotNull Integer userId,
         @NotBlank String categoryCode,
         @NotNull Integer mungpleId,
-        @NotBlank String placeName,
+        @NotNull String placeName,
         @NotBlank String description,
         String latitude,
         String longitude) {
