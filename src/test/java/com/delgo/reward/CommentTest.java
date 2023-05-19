e5386c3223884c5032e5cbfc0d0d60bca69029d6
Gupuroom <cjsrnr1114@gmail.com>
2023-05-19 13:50:50 +0900

    ADD : 댓글 개수 정상화 해주는 TEST 코드 추가

---
+++ src/test/java/com/delgo/reward/CommentTest.java
@@ -0,0 +1,44 @@
+package com.delgo.reward;
+
+import com.delgo.reward.domain.certification.Certification;
+import com.delgo.reward.repository.CertRepository;
+import com.delgo.reward.repository.CommentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@Slf4j
+@SpringBootTest
+public class CommentTest {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private CertRepository certRepository;
+
+    @Test
+    public void makeCommentCountTEST() {
+        //given
+
+        //when
+        List<Certification> certifications = certRepository.findAll();
+        for(Certification cert : certifications){
+            // commentCount 업데이트
+            int commentCount = commentRepository.countCommentByCertId(cert.getCertificationId());
+            if(commentCount > 0) {
+                System.out.println("certId : " + cert.getCertificationId());
+                System.out.println("commentCount : " + commentCount);
+                cert.setCommentCount(commentCount);
+            }
+        }
+
+        //then
+    }
+}
