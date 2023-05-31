17e6056d38660234c5c70b0007d8d9d30247947e
gupuroom <cjsrnr1114@naver.com>
2023-05-31 16:18:15 +0900

    ADD: Munpgle Detail Photo CrawlingTest 중

--- src/test/java/com/delgo/reward/CrawlingTest.java
+++ src/test/java/com/delgo/reward/CrawlingTest.java
@@ -13,14 +13,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CrawlingTest {
 
     @Autowired
-    private GetNaverMungplePhotoCrawlingService naverCrawlingService;
+    private GetNaverMungplePhotoCrawlingService getNaverMungplePhotoCrawlingService;
 
-    @Autowired
 
     @Test
     public void getNaverMungpleDetailPhotoCrawlingTest() throws Exception {
-        String url = "https://map.naver.com/v5/search/%EB%B9%84%EC%8A%A4%ED%8A%B8%EB%A1%9C%EB%A7%88%EC%9D%B8/place/1047172387?c=15,0,0,0,dh&isCorrectAnswer=true";
+        String url = "https://map.naver.com";
 
-        naverCrawlingService.crawlingProcess(url);
+        getNaverMungplePhotoCrawlingService.crawlingProcess(url);
     }
+
+//    @Test
+//    public void getCrawlingTest3() throws Exception {
+//        String url = "https://map.naver.com/v5/search/인천역?c=15,0,0,0,dh&isCorrectAnswer=true";
+//
+//        naverCrawlingService.crawlingProcess(url);
+//    }
 }
