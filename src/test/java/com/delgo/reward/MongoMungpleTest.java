255444d9c385fed196fbc68b9b2d7ad48d426a2e
Gupuroom <cjsrnr1114@naver.com>
2023-06-04 01:23:07 +0900

    ADD: Mungple Detail 크롤링 추가 - 검색 시 장소 하나 or 두개 이상 크롤링 따로 해야함.

--- src/test/java/com/delgo/reward/MongoMungpleTest.java
+++ src/test/java/com/delgo/reward/MongoMungpleTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,8 +29,33 @@ public class MongoMungpleTest {
 //    }
 
     @Test
-    public void modifyMungpleDetailTest() {
-        mongoMungpleService.modifyMungpleDetail(2,"똑똑이2");
+    public void modifyEnterDescTest() {
+        int mungpleId = 5;
+        String desc = "상주견 모르네가 대형견을 무서워해서 대형견 친구들은 아쉽게도 함께 하지 못해요.\n" +
+                "카페앞 1대 주차 할 수 있어요. 미리 전화를 하고 오시면 주차 여부를 안내해 드릴 수 있어요.\n" +
+                "펫티캣을 지켜서 반려인 비반려인 모두 행복 할 수 있는 공간을 만들어주세요.";
+
+
+        mongoMungpleService.modifyEnterDesc(mungpleId, desc);
+    }
+
+    @Test
+    public void modifyPhotoUrlsTest() {
+        int mungpleId = 2;
+        List<String> photos = new ArrayList<>();
+        photos.add("https://test.com");
+
+
+        mongoMungpleService.modifyPhotoUrls(mungpleId, photos);
+    }
+
+    @Test
+    public void modifyMenuPhotoUrlsTest() {
+        int mungpleId = 2;
+        String url = "https:asfsadf";
+
+
+        mongoMungpleService.modifyMenuPhotoUrl(mungpleId, url);
     }
 
     @Test
