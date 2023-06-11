90c50fb3e03b937bfcbf728eb45ca9357b1013dd
danpung2 <danpung_2@naver.com>
2023-06-11 18:33:17 +0900

    ADD: 리액션 도메인 정의 (maria)

---
+++ src/main/java/com/delgo/reward/domain/certification/Reaction.java
@@ -0,0 +1,22 @@
+package com.delgo.reward.domain.certification;
+
+import com.delgo.reward.comm.code.ReactionCode;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reactionId;
+    private Integer certificationId;
+    @Enumerated(EnumType.STRING)
+    private ReactionCode reactionCode;
+    private Integer userId;
+}
