158c5198a95074dc6c935f1dacd175a9dc055605
Gupuroom <cjsrnr1114@gmail.com>
2023-06-13 16:40:13 +0900

    ADD: 주소 가리기 기능 추가 : isHideAddress 추가 - 작성자의 경우 주소 표시되어야 하므로 isOwner로 작성자 여부 반환

--- src/main/java/com/delgo/reward/dto/cert/CertResDTO.java
+++ src/main/java/com/delgo/reward/dto/cert/CertResDTO.java
@@ -6,23 +6,29 @@ import com.delgo.reward.domain.like.LikeList;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 public class CertResDTO {
     private Integer certificationId;
     private String placeName; // 장소 명
     private String description; // 내용
-    private String address; // 주소
     private String photoUrl; // 사진 URL
     private int commentCount; // 댓글 개수
 
+    private Boolean isHideAddress; // 주소 숨김 여부
+    private Boolean isOwner; // cert 작성자인가?
+    private String address; // 주소
+
+
     private Integer userId; // 작성자 ID
-    private String userName; // 작성자 이름
+    private String userName; // 작성자 이 름
     private String userProfile; // 작성자 프로필
 
     private Boolean isLike = false; // 내가 좋아요를 눌렀는가?
@@ -34,21 +40,23 @@ public class CertResDTO {
     public CertResDTO(Certification certification, Integer ownerId) {
         this(certification);
         if(certification.getLikeLists() != null) {
+            isOwner = certification.getUser().getUserId() == ownerId;
             likeCount = (int) certification.getLikeLists().stream().filter(LikeList::isLike).count();
             isLike = certification.getLikeLists().stream().anyMatch(likeList -> likeList.getUserId().equals(ownerId) && likeList.isLike());
         }
     }
 
-    public CertResDTO(Certification certification) {
-        certificationId = certification.getCertificationId();
-        userId = certification.getUser().getUserId();
-        userName = certification.getUser().getName();
-        userProfile = certification.getUser().getProfile();
-        placeName = certification.getPlaceName();
-        description = certification.getDescription();
-        address = certification.getAddress();
-        photoUrl = certification.getPhotoUrl();
-        commentCount = certification.getCommentCount();
-        registDt = certification.getRegistDt();
+    public CertResDTO(Certification cert) {
+        certificationId = cert.getCertificationId();
+        userId = cert.getUser().getUserId();
+        userName = cert.getUser().getName();
+        userProfile = cert.getUser().getProfile();
+        placeName = cert.getPlaceName();
+        description = cert.getDescription();
+        address =  cert.getAddress();
+        isHideAddress =  cert.getIsHideAddress();
+        photoUrl = cert.getPhotoUrl();
+        commentCount = cert.getCommentCount();
+        registDt = cert.getRegistDt();
     }
 }
