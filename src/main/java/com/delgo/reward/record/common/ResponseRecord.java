763eb646d42e39af9a933d16558564b16b4ac537
Gupuroom <cjsrnr1114@gmail.com>
2023-06-23 14:43:12 +0900

    ADD: Swagger 설정

--- src/main/java/com/delgo/reward/record/common/ResponseRecord.java
+++ src/main/java/com/delgo/reward/record/common/ResponseRecord.java
@@ -1,3 +1,6 @@
 package com.delgo.reward.record.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ResponseRecord<T>(int code, String codeMsg, T data) {}
