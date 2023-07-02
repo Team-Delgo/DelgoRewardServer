25fec4dad937114afdb050075dfeb93f40425816
gupuroom <cjsrnr1114@naver.com>
2023-07-02 16:14:41 +0900

    FIX: CA0006 병원 - 디테일 페이지 추가
    1. MungpleDetailResDTO 분기 처리
    - CafeDetailResDTO ( 카페, 식당 사용 )
    - PriceTagDetailResDTO ( 병원, 미용실 등 사용 )
    
    2.MungpleDetail Domain 수정
    - price Tag 관련 컬럼 추가

---
+++ src/main/java/com/delgo/reward/dto/mungple/PriceTagDetailResDTO.java
@@ -0,0 +1,24 @@
+package com.delgo.reward.dto.mungple;
+
+import com.delgo.reward.domain.Mungple;
+import com.delgo.reward.mongoDomain.MungpleDetail;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+
+@Getter
+@ToString
+public class PriceTagDetailResDTO extends MungpleDetailResDTO {
+    private final Boolean isPriceTag; // 가격표 존재 여부
+    private final List<String> priceTagPhotoUrls; // 가격표 사진
+
+
+    public PriceTagDetailResDTO(Mungple mungple, MungpleDetail mungpleDetail, int certCount){
+        super(mungple,mungpleDetail,certCount);
+
+        isPriceTag = mungpleDetail.getIsPriceTag();
+        priceTagPhotoUrls = mungpleDetail.getPriceTagPhotoUrls();
+    }
+}
--- src/main/java/com/delgo/reward/mongoDomain/MungpleDetail.java
+++ src/main/java/com/delgo/reward/mongoDomain/MungpleDetail.java
@@ -18,6 +18,7 @@ import java.util.Map;
 @AllArgsConstructor
 @Document(collection="mungple_detail")
 public class MungpleDetail {
+    // Common
     @Id
     private String id;
     @Field("mungple_id")
@@ -28,20 +29,10 @@ public class MungpleDetail {
 
     @Field("enter_desc")
     private String enterDesc; // 강아지 동반 안내 매장 설명문
-    @Field("business_hour")
-    private Map<BusinessHourCode, String> businessHour; // 운영 시간 ( 요일별로 표시 )
     @Field("accept_size")
     private Map<String, DetailCode> acceptSize;  // 허용 크기 ( S, M , L )
-
-    @Field("resident_dog_name")
-    private String residentDogName; // 상주견 이름
-    @Field("resident_dog_photo")
-    private String residentDogPhoto; // 상주견 사진
-
-    @Field("represent_menu_title")
-    private String representMenuTitle; // 대표 메뉴 제목
-    @Field("represent_menu_photo_urls")
-    private List<String> representMenuPhotoUrls; // 대표 메뉴 URL List // ※무조건 3개 이상이어야 함.
+    @Field("business_hour")
+    private Map<BusinessHourCode, String> businessHour; // 운영 시간 ( 요일별로 표시 )
 
     @Field("insta_id")
     private String instaId; // 인스타 ID
@@ -55,4 +46,21 @@ public class MungpleDetail {
     private String editorNoteUrl; // 에디터 노트 URL
     @Field("copy_link")
     private String copyLink;
+
+    // CA0002, CA0003
+    @Field("resident_dog_name")
+    private String residentDogName; // 상주견 이름
+    @Field("resident_dog_photo")
+    private String residentDogPhoto; // 상주견 사진
+
+    @Field("represent_menu_title")
+    private String representMenuTitle; // 대표 메뉴 제목
+    @Field("represent_menu_photo_urls")
+    private List<String> representMenuPhotoUrls; // 대표 메뉴 URL List // ※무조건 3개 이상이어야 함.
+
+    // CA0001, CA0005, CA0006, CA0007
+    @Field("is_price_tag")
+    private Boolean isPriceTag; // 가격표 존재 여부
+    @Field("price_tag_photo_urls")
+    private List<String> priceTagPhotoUrls; // 가격표 사진
 }
