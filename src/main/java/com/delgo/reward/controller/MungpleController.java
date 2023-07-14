eec9681bc3b6033c37c9127ef6ba43fb950b368c
Gupuroom <cjsrnr1114@gmail.com>
2023-07-14 09:24:56 +0900

    FIX: Mungple Controller 주석 및 메서드명 변경 - 코드 컨벤션에 맞게 수정

--- src/main/java/com/delgo/reward/controller/MungpleController.java
+++ src/main/java/com/delgo/reward/controller/MungpleController.java
@@ -24,45 +24,44 @@ public class MungpleController extends CommController {
     private final MungpleService mungpleService;
     private final MongoMungpleService mongoMungpleService;
 
-    /*
-     * 멍플 등록
-     * Request Data : MungpleDTO
-     * - 주소로 위도, 경도 구할 수 있어야 함.
-     * Response Data : 등록한 멍플 데이터 반환
+    /**
+     * Mungple 생성
+     * @param record, photo
+     * @return MungpleResDTO
      */
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity register(@Validated @RequestPart(value = "data") MungpleRecord record, @RequestPart(required = false) MultipartFile photo) {
+    public ResponseEntity createMungple(@Validated @RequestPart(value = "data") MungpleRecord record, @RequestPart(required = false) MultipartFile photo) {
         return mungpleService.isMungpleExisting(record.address())
-                ? ErrorReturn(APICode.MUNGPLE_DUPLICATE_ERROR) // 중복 에러
+                ? ErrorReturn(APICode.MUNGPLE_DUPLICATE_ERROR)
                 : SuccessReturn(mungpleService.register(record, photo));
     }
 
-    /*
-     * 멍플 카테고리 별 조회
-     * Request Data : categoryCodeF
-     * - CA0000 = 전체 조회
-     * Response Data : 카테고리별 멍플 리스트 반환
+    /**
+     * [Category] Mungple 조회 - (CA0000: 전체 조회)
+     * @param categoryCode
+     * @return List<MungpleResDTO>
+     *
      */
     @GetMapping(value={"/category/{categoryCode}","/category"})
-    public ResponseEntity getCategory(@PathVariable String categoryCode) {
+    public ResponseEntity getMungplesByCategory(@PathVariable String categoryCode) {
         if (categoryCode.isBlank()) return ParamErrorReturn("categoryCode"); // Validate - Blank Check
         return SuccessReturn(mungpleService.getMungpleByCategoryCode(categoryCode));
     }
 
-    /*
-     * Mungple 인증 개수 많은 순으로 조회 ( 추후 광고 관련 코드 넣을 예정 )
-     * Request Data : count
-     * Response Data :
+    /**
+     * [인증 개수 많은 순] Mungple 조회 - (추후 광고 관련 코드 넣을 예정)
+     * @param count
+     * @return List<MungpleResDTO>
+     *
      */
     @GetMapping("/top")
-    public ResponseEntity getTopData(@RequestParam Integer count) {
+    public ResponseEntity getMungpleOfMostCertCount(@RequestParam Integer count) {
         return SuccessReturn(mungpleService.getMungpleOfMostCount(count));
     }
 
-    /*
+    /**
      * Mungple 삭제
-     * Request Data : mungpleId
-     * Response Data :
+     * @param mungpleId
      */
     @DeleteMapping(value={"/{mungpleId}",""})
     public ResponseEntity deleteMungple(@PathVariable Integer mungpleId) {
@@ -73,25 +72,25 @@ public class MungpleController extends CommController {
     // -------------------------------------- DETAIL --------------------------------------
 
     /**
-     * 멍플 Id로 디테일 데이터 조회
-     * @param mungpleId
-     * @return 디테일 데이터
-     */
-    @GetMapping("/detail")
-    public ResponseEntity getDetailDataByMungpleId(@RequestParam int mungpleId){
-        return SuccessReturn(mongoMungpleService.getMungpleDetailDataByMungpleId(mungpleId));
-    }
-
-    /*
      * Mungple Detail 등록
-     * Request Data : mungpleId
-     * Response Data :
+     * @param record
+     * @return MungpleDetailResDTO
      */
     @PostMapping("/detail")
-    public ResponseEntity createDetailData(@RequestBody MungpleDetailRecord record){
+    public ResponseEntity createMungpleDetail(@RequestBody MungpleDetailRecord record){
         if(mongoMungpleService.isExist(record.mungpleId()))
             return ErrorReturn(APICode.MUNGPLE_DUPLICATE_ERROR);
 
         return SuccessReturn(mongoMungpleService.createMungpleDetail(record));
+    }
+
+    /**
+     * [MungpleId] Mungple Detail 조회
+     * @param mungpleId
+     * @return MungpleDetailResDTO
+     */
+    @GetMapping("/detail")
+    public ResponseEntity getMungpleDetailByMungpleId(@RequestParam int mungpleId){
+        return SuccessReturn(mongoMungpleService.getMungpleDetailDataByMungpleId(mungpleId));
     }
 }
