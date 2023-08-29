6f5ace958f53380e400cfe017e993516b6e82770
Gupuroom <cjsrnr1114@gmail.com>
2023-08-29 16:49:12 +0900

    ADD: NCP GreenEye 유해사진 판별 추가 - URL로 판별 - TestCode 추가

---
+++ src/main/java/com/delgo/reward/comm/ncp/greeneye/GreenEyeResDTO.java
@@ -0,0 +1,19 @@
+package com.delgo.reward.comm.ncp.greeneye;
+
+
+import lombok.*;
+
+import java.util.List;
+
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GreenEyeResDTO {
+    private double adult;
+    private double normal;
+    private double porn;
+    private double sexy;
+}
