dbc2bce58d0e9a11a4cf81defddeabf47801039a
gupuroom <cjsrnr1114@naver.com>
2023-08-30 16:12:23 +0900

    ADD: migrationPhotoData Test 코드 추가 - CertPhoto 로 사진 데이터 마이그레이션

--- src/test/java/com/delgo/reward/CertificationTest.java
+++ src/test/java/com/delgo/reward/CertificationTest.java
@@ -1,6 +1,9 @@
 package com.delgo.reward;
 
 
+import com.delgo.reward.domain.certification.CertPhoto;
+import com.delgo.reward.domain.certification.Certification;
+import com.delgo.reward.repository.CertPhotoRepository;
 import com.delgo.reward.repository.CertRepository;
 import com.delgo.reward.service.CertService;
 import org.junit.Test;
@@ -9,15 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CertificationTest {
 
     @Autowired
-    private CertService certificationService;
+    private CertService certService;
     @Autowired
     private CertRepository certRepository;
+    @Autowired
+    private CertPhotoRepository certPhotoRepository;
 
     @Test
     public void DeleteTest() {
@@ -25,8 +32,24 @@ public class CertificationTest {
         int certificationId = 935;
 
         //when
-        certificationService.deleteCert(certificationId);
+        certService.deleteCert(certificationId);
 
         //then
+    }
+
+    @Test
+    public void migrationPhotoData(){
+        List<Certification> certs = certRepository.findAll();
+        for(Certification cert : certs){
+
+            String photoUrl = cert.getPhotoUrl();
+            CertPhoto certPhoto = CertPhoto.builder()
+                    .certificationId(cert.getCertificationId())
+                    .url(photoUrl)
+                    .isCorrect(cert.getIsCorrect())
+                    .build();
+
+            certPhotoRepository.save(certPhoto);
+        }
     }
 }
