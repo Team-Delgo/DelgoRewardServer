51ab500c91cd7455cbe4ff073da8dc38b40f15c3
Gupuroom <cjsrnr1114@gmail.com>
2023-09-07 14:42:13 +0900

    ADD: checkSaveConfirm 추가
    등록 로직
    1. Sheet에서 Data 조회
    2. isRegist = false인 값 MongoDB에 저장
    3. checkSaveConfirm() 호출
    4. Sheet에 있는 isRegist = true 로 변경
    5. Sheet에 있는 등록날짜 = 오늘날짜로 변경
    
    * MongoMungpleTest 수정

--- src/main/java/com/delgo/reward/comm/sheets/SheetDTO.java
+++ src/main/java/com/delgo/reward/comm/sheets/SheetDTO.java
@@ -10,12 +10,13 @@ import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class SheetDTO {
-    private String isRegist; // 등록 여부
+    private Boolean isRegist; // 등록 여부
     private String createDate; // 등록날짜
     private String placeName; // 업체명(상호명)
     private String placeNameEn; // 업체명(상호명)
@@ -27,26 +28,32 @@ public class SheetDTO {
     private String acceptSize; // 강아지 동반 안내
     private String enterDesc; // 강아지 동반 안내 매장 설명문
     private String parkingInfo; // 주차 정보
-    private String isParking; // 주차 공간
+    private Boolean isParking; // 주차 공간
     private String representMenuTitle; // 강아지대표매뉴
 
     public SheetDTO(List<Object> row) {
-        this.isRegist = !row.isEmpty() && row.get(0) != null ? (String) row.get(0) : "";
-        this.createDate = row.size() > 1 && row.get(1) != null ? (String) row.get(1) : "";
-        this.placeName = row.size() > 2 && row.get(2) != null ? (String) row.get(2) : "";
-        this.placeNameEn = row.size() > 3 && row.get(3) != null ? (String) row.get(3) : "";
-        this.address = row.size() > 4 && row.get(4) != null ? (String) row.get(4) : "";
-        this.phoneNo = row.size() > 5 && row.get(5) != null ? (String) row.get(5) : "";
-        this.businessHour = row.size() > 6 && row.get(6) != null ? (String) row.get(6) : "";
-        this.residentDogName = row.size() > 7 && row.get(7) != null ? (String) row.get(7) : "";
-        this.instaId = row.size() > 8 && row.get(8) != null ? (String) row.get(8) : "";
-        this.acceptSize = row.size() > 9 && row.get(9) != null ? (String) row.get(9) : "";
-        this.enterDesc = row.size() > 10 && row.get(10) != null ? (String) row.get(10) : "";
-        this.parkingInfo = row.size() > 11 && row.get(11) != null ? (String) row.get(11) : "";
-        this.isParking = row.size() > 12 && row.get(12) != null ? (String) row.get(12) : "";
-        this.representMenuTitle = row.size() > 13 && row.get(13) != null ? (String) row.get(13) : "";
+        this.isRegist = getValue(row, 0).map(Boolean::parseBoolean).orElse(false);
+        this.createDate = getValue(row, 1).orElse("");
+        this.placeName = getValue(row, 2).orElse("");
+        this.placeNameEn = getValue(row, 3).orElse("");
+        this.address = getValue(row, 4).orElse("");
+        this.phoneNo = getValue(row, 5).orElse("");
+        this.businessHour = getValue(row, 6).orElse("");
+        this.residentDogName = getValue(row, 7).orElse("");
+        this.instaId = getValue(row, 8).orElse("");
+        this.acceptSize = getValue(row, 9).orElse("");
+        this.enterDesc = getValue(row, 10).orElse("");
+        this.parkingInfo = getValue(row, 11).orElse("");
+        this.isParking = getValue(row, 12).map(Boolean::parseBoolean).orElse(false);
+        this.representMenuTitle = getValue(row, 13).orElse("");
     }
 
+    private Optional<String> getValue(List<Object> row, int index) {
+        if (index < row.size() && row.get(index) != null) {
+            return Optional.of((String) row.get(index));
+        }
+        return Optional.empty();
+    }
 
     public MongoMungple toMongoEntity(CategoryCode categoryCode, Location location){
         return MongoMungple.builder()
@@ -66,7 +73,7 @@ public class SheetDTO {
                         Double.parseDouble(location.getLongitude()),
                         Double.parseDouble(location.getLatitude())))
                 .instaId(instaId)
-                .isParking(Boolean.valueOf(isParking))
+                .isParking(isParking)
                 .parkingInfo(parkingInfo)
                 .residentDogName(residentDogName)
                 .representMenuTitle(representMenuTitle)
--- src/main/java/com/delgo/reward/comm/sheets/SheetsService.java
+++ src/main/java/com/delgo/reward/comm/sheets/SheetsService.java
@@ -21,11 +21,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -38,34 +41,52 @@ public class SheetsService {
     private final GeoService geoService;
     private final MongoMungpleService mongoMungpleService;
 
+    private Sheets sheets;
     private final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private final String SHEET_ID ="10wrl07KdizmU2Z1ntSTVjbbYGV6V22tUMRJjjmDTBv4";
 
-    public void getSheetsData(CategoryCode categoryCode) throws IOException, GeneralSecurityException {
+    @PostConstruct
+    public void connectSheet() throws GeneralSecurityException, IOException {
         NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
 
-        String sheetId = "10wrl07KdizmU2Z1ntSTVjbbYGV6V22tUMRJjjmDTBv4";
-        String sheetName = categoryCode.getCode() + "_LIST";
-        String range = sheetName + "!A1:M300";
-
-        Sheets sheetsService = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        sheets = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName("Google Sheets API")
                 .build();
+    }
 
-        ValueRange response = sheetsService.spreadsheets().values()
-                .get(sheetId, range)
+    public void getSheetsData(CategoryCode categoryCode) throws IOException, GeneralSecurityException {
+        String range = generateRangeString("A", 1, "M", 300);
+
+        ValueRange response = sheets.spreadsheets().values()
+                .get(SHEET_ID, categoryCode.getSheetName() + "!" + range)
                 .execute();
 
         List<List<Object>> values = response.getValues();
-        for(int i=1; i< values.size(); i++){ // 1 행은 Filter 행 따라서 패싱 한다.
-            List<Object> row = values.get(i);
+        for (int rowNum = 1; rowNum < values.size(); rowNum++) { // 1 행은 Filter 행 따라서 패싱 한다.
+            List<Object> row = values.get(rowNum);
             SheetDTO sheet = new SheetDTO(row);
+            if (!sheet.getIsRegist()) {
+                MongoMungple mongoMungple = sheet.toMongoEntity(categoryCode, geoService.getGeoData(sheet.getAddress()));
+                mongoMungple.setAcceptSize(sheet.getAcceptSize());
+                mongoMungple.setBusinessHour(sheet.getBusinessHour());
 
-            MongoMungple mongoMungple = sheet.toMongoEntity(categoryCode, geoService.getGeoData(sheet.getAddress()));
-            mongoMungple.setAcceptSize(sheet.getAcceptSize());
-            mongoMungple.setBusinessHour(sheet.getBusinessHour());
-
-            mongoMungpleService.save(mongoMungple);
+                checkSaveConfirm(categoryCode, rowNum + 1); // values = 0부터 시작 따라서 + 1 해줘야 함.
+                mongoMungpleService.save(mongoMungple);
+            }
         }
+    }
+
+    public void checkSaveConfirm(CategoryCode categoryCode, int rowNum) throws IOException, GeneralSecurityException {
+        String range = generateRangeString("A", rowNum, "B", rowNum);
+
+        // 등록 여부(A)에 "True", 등록 날짜(B)에 현재 날짜를 설정
+        List<Object> dataRow = Arrays.asList("TRUE", LocalDate.now().toString());
+        ValueRange body = new ValueRange().setValues(List.of(dataRow));
+
+        sheets.spreadsheets().values()
+                .update(SHEET_ID, categoryCode.getSheetName() + "!" + range, body)
+                .setValueInputOption("RAW")
+                .execute();
     }
 
     private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
@@ -75,11 +96,15 @@ public class SheetsService {
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets,
-                Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY))
+                Collections.singletonList(SheetsScopes.SPREADSHEETS))
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public String generateRangeString(String startColumn, int startRow, String endColumn, int endRow) {
+        return startColumn + startRow + ":" + endColumn + endRow;
     }
 }
