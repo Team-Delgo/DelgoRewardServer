0d0bda9aeec3be269ee8074f2617546837a3f0ed
gupuroom <cjsrnr1114@naver.com>
2023-09-10 16:47:50 +0900

    ADD: Other Page Map API 추가 - OtherMapDTO 추가 - Other Map 조회 시에는 올바른 인증만 조회 한다.

--- src/main/java/com/delgo/reward/service/MapService.java
+++ src/main/java/com/delgo/reward/service/MapService.java
@@ -4,6 +4,7 @@ package com.delgo.reward.service;
 import com.delgo.reward.comm.code.CategoryCode;
 import com.delgo.reward.domain.certification.Certification;
 import com.delgo.reward.dto.cert.CertByMungpleResDTO;
+import com.delgo.reward.dto.map.OtherMapDTO;
 import com.delgo.reward.mongoService.MongoMungpleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,11 +22,11 @@ import java.util.stream.Collectors;
 public class MapService {
 
     private final CertService certService;
-//    private final MungpleService mungpleService;
+    private final UserService userService;
     private final MongoMungpleService mongoMungpleService;
 
     public Map<String, Object> getMap(int userId) {
-        List<CertByMungpleResDTO> certifications = certService.getCertsByUserId(userId).stream().map(c -> new CertByMungpleResDTO(c,userId)).toList();  // 인증 리스트 조회
+        List<CertByMungpleResDTO> certs = certService.getCertsByUserId(userId).stream().map(c -> new CertByMungpleResDTO(c,userId)).toList();  // 인증 리스트 조회
         List<CertByMungpleResDTO> exposedCertList = certService.getExposedCerts(3).stream().map(c -> new CertByMungpleResDTO(c,userId)).toList();  // 노출시킬 인증 리스트 조회
 
         return (userId == 0)
@@ -33,10 +34,18 @@ public class MapService {
                 "exposedNormalCertList", exposedCertList.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()),
                 "exposedMungpleCertList", exposedCertList.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList()))
                 : Map.of("mungpleList", mongoMungpleService.getActiveMungpleByCategoryCode(CategoryCode.TOTAL.getCode()),
-                "normalCertList", certifications.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()),
-                "mungpleCertList", certifications.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList()),
+                "normalCertList", certs.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()),
+                "mungpleCertList", certs.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList()),
                 "exposedNormalCertList", exposedCertList.stream().filter(c -> c.getMungpleId() == 0).collect(Collectors.toList()),
                 "exposedMungpleCertList", exposedCertList.stream().filter(c -> c.getMungpleId() != 0).collect(Collectors.toList()));
+    }
+
+    public OtherMapDTO getOtherMap(int userId) {
+        return new OtherMapDTO(
+                userService.getUserById(userId),
+                certService.getCorrectCertsByUserId(userId).stream().map(c -> new CertByMungpleResDTO(c,userId)).toList(),  // 인증 리스트 조회
+                certService.getCorrectCertCountByUserId(userId)
+        );
     }
 
     public Map<String, List<Certification>> test() {
