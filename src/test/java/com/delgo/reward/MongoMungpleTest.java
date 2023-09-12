9bd40461ea3381747565331bf5262e85e5d2a768
Gupuroom <cjsrnr1114@gmail.com>
2023-09-12 14:54:25 +0900

    FIX: 에러나는 Mungple test 삭제

--- src/test/java/com/delgo/reward/MongoMungpleTest.java
+++ src/test/java/com/delgo/reward/MongoMungpleTest.java
@@ -1,25 +1,17 @@
 package com.delgo.reward;
 
 
-import com.delgo.reward.domain.mungple.Mungple;
-import com.delgo.reward.mongoDomain.mungple.MongoMungple;
-import com.delgo.reward.mongoDomain.mungple.MungpleDetail;
 import com.delgo.reward.mongoRepository.MongoMungpleRepository;
 import com.delgo.reward.mongoRepository.MungpleDetailRepository;
 import com.delgo.reward.mongoService.MongoMungpleService;
-import com.delgo.reward.repository.MungpleRepository;
-import com.delgo.reward.service.ExcelParserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,69 +22,15 @@ public class MongoMungpleTest {
 
     @Autowired
     private MongoMungpleRepository mongoMungpleRepository;
-    @Autowired
-    private MungpleRepository mungpleRepository;
+
     @Autowired
     private MungpleDetailRepository mungpleDetailRepository;
-
-    @Autowired
-    private ExcelParserService excelParserService;
-
-//    @Test
-//    public void makeMungpletoMongoTEST() {
-//        mongoMungpleService.makeMungpletoMongo();
-//    }
 
     @Test
     public void deleteAllMongoMunple(){
         mongoMungpleRepository.deleteAll();
     }
-    @Test
-    public void saveAllMungpleAndDetailToMongo(){
-        List<Mungple> mungpleList = mungpleRepository.findAll();
-        for (Mungple mungple : mungpleList){
-            Optional<MungpleDetail> mungpleDetailOptional = mungpleDetailRepository.findByMungpleId(mungple.getMungpleId());
-            if(mungpleDetailOptional.isEmpty())
-                continue;
-            MungpleDetail mungpleDetail = mungpleDetailOptional.get();
 
-            MongoMungple mongoMungple = MongoMungple
-                    .builder()
-                    .categoryCode(mungple.getCategoryCode())
-                    .phoneNo(mungple.getPhoneNo())
-                    .placeName(mungple.getPlaceName())
-                    .placeNameEn(mungple.getPlaceNameEn())
-                    .roadAddress(mungple.getRoadAddress())
-                    .jibunAddress(mungple.getJibunAddress())
-                    .geoCode(mungple.getGeoCode())
-                    .pGeoCode(mungple.getPGeoCode())
-                    .latitude(mungple.getLatitude())
-                    .longitude(mungple.getLongitude())
-                    .photoUrl(mungple.getPhotoUrl())
-                    .detailUrl(mungple.getDetailUrl())
-                    .isActive(mungple.isActive())
-                    .createdAt(LocalDateTime.now())
-                    .location(new GeoJsonPoint(Double.parseDouble(mungple.getLongitude()), Double.parseDouble(mungple.getLatitude())))
-                    .photoUrls(mungpleDetail.getPhotoUrls())
-                    .enterDesc(mungpleDetail.getEnterDesc())
-                    .acceptSize(mungpleDetail.getAcceptSize())
-                    .businessHour(mungpleDetail.getBusinessHour())
-                    .instaId(mungpleDetail.getInstaId())
-                    .isParking(mungpleDetail.getIsParking())
-                    .parkingInfo(mungpleDetail.getParkingInfo())
-                    .editorNoteUrl(mungpleDetail.getEditorNoteUrl())
-                    .copyLink(mungpleDetail.getCopyLink())
-                    .residentDogName(mungpleDetail.getResidentDogName())
-                    .residentDogPhoto(mungpleDetail.getResidentDogPhoto())
-                    .representMenuTitle(mungpleDetail.getRepresentMenuTitle())
-                    .representMenuPhotoUrls(mungpleDetail.getRepresentMenuPhotoUrls())
-                    .isPriceTag(mungpleDetail.getIsPriceTag())
-                    .priceTagPhotoUrls(mungpleDetail.getPriceTagPhotoUrls())
-                    .build();
-
-            mongoMungpleRepository.save(mongoMungple);
-        }
-    }
 
     @Test
     public void modifyPhotoUrlsTest() {
