43a9e9fc1101c6b117f95fb85aa65fa03a2912e6
Gupuroom <cjsrnr1114@gmail.com>
2023-09-21 09:46:55 +0900

    ADD: 멍플 정렬 조건 추가 [전략 패턴 사용] - 내가 저장한 멍플 조회 시 정렬 조건 별로 조회 결과 다름  * 거리순  * 인증 많은 순  * 저장 많이 한 순
     *** Mungple 조회 시에도 동일한 정렬 조건을 적용하기 위해 전략 패턴을 사용해서 구현
    
     - MungpleSort Enum 추가
     - 추후 Test Code 추가 예정

---
+++ src/main/java/com/delgo/reward/service/strategy/BookmarkCountSorting.java
@@ -0,0 +1,33 @@
+package com.delgo.reward.service.strategy;
+
+import com.delgo.reward.mongoDomain.mungple.MongoMungple;
+import com.delgo.reward.repository.BookmarkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class BookmarkCountSorting implements MungpleSortingStrategy {
+    private final BookmarkRepository bookmarkRepository;
+
+    @Override
+    public List<MongoMungple> sort(List<MongoMungple> mungpleList) {
+        // MariaDB에서 각 mungpleId별 Bookmark 개수 조회
+        Map<MongoMungple, Integer> countMap = new HashMap<>();
+        for(MongoMungple mungple : mungpleList) {
+            int count = bookmarkRepository.countOfActiveBookmarkByMungple(mungple.getMungpleId());
+            countMap.put(mungple, count);
+        }
+
+        // 저장 개수에 따라 mungpleId 리스트 정렬
+        return mungpleList.stream()
+                .sorted(Comparator.comparing(countMap::get))
+                .collect(Collectors.toList());
+    }
+}
