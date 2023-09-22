c62051b79f96ab8d290148df0654abed407581d0
Gupuroom <cjsrnr1114@gmail.com>
2023-09-22 15:34:56 +0900

    DEL: Survey 관련 코드 삭제 - 사용 안 함.

--- src/main/java/com/delgo/reward/repository/VersionRepository.java
+++ src/main/java/com/delgo/reward/repository/VersionRepository.java
@@ -2,13 +2,13 @@ package com.delgo.reward.repository;
 
 
 import com.delgo.reward.domain.Version;
-import com.delgo.reward.domain.survey.Survey;
+import com.delgo.reward.domain.certification.Certification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
 
-public interface VersionRepository extends JpaRepository<Version, Integer>, JpaSpecificationExecutor<Survey> {
+public interface VersionRepository extends JpaRepository<Version, Integer>, JpaSpecificationExecutor<Version> {
     Optional<Version> findByIsActive(boolean isActive);
 }
