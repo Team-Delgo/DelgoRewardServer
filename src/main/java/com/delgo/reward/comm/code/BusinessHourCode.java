4a2a91421481a8b6dc011ea279c7fab8f5e55481
Gupuroom <cjsrnr1114@gmail.com>
2023-09-25 09:18:50 +0900

    ADD: BusinessHourCode Swagger 주석 추가

--- src/main/java/com/delgo/reward/comm/code/BusinessHourCode.java
+++ src/main/java/com/delgo/reward/comm/code/BusinessHourCode.java
@@ -1,9 +1,23 @@
 package com.delgo.reward.comm.code;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
+@Getter
+@Schema(enumAsRef = true, description =
+        "영업 시간: \n" +
+                "* `MON` - 월요일\n" +
+                "* `TUE` - 화요일\n" +
+                "* `WED` - 수요일\n" +
+                "* `THU` - 목요일\n" +
+                "* `FRI` - 금요일\n" +
+                "* `SAT` - 토요일\n" +
+                "* `SUN` - 일요일\n" +
+                "* `HOLIDAY` - 휴일\n" +
+                "* `BREAK_TIME` - 쉬는 시간\n" +
+                "* `LAST_ORDER` - 마지막 주문 시간\n")
 public enum BusinessHourCode {
-
     MON("MON", "월요일","휴무"),
     TUE("TUE", "화요일","휴무"),
     WED("WED", "수요일","휴무"),
@@ -23,18 +37,6 @@ public enum BusinessHourCode {
         this.code = code;
         this.desc = desc;
         this.defaultValue = defaultValue;
-    }
-
-    public String getCode(){
-        return this.code;
-    }
-
-    public String getDesc() {
-        return this.desc;
-    }
-
-    public String getDefaultValue() {
-        return this.defaultValue;
     }
 
     // @RequestParam ENUM Parsing
