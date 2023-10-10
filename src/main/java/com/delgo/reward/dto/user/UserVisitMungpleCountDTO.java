95c704cf21f510acca166537a5aa95cfeab51497
Gupuroom <cjsrnr1114@gmail.com>
2023-10-10 15:09:03 +0900

    ADD: AccountController Swagger 추가

--- src/main/java/com/delgo/reward/dto/user/UserVisitMungpleCountDTO.java
+++ src/main/java/com/delgo/reward/dto/user/UserVisitMungpleCountDTO.java
@@ -1,25 +1,27 @@
 package com.delgo.reward.dto.user;
 
-import com.delgo.reward.mongoDomain.mungple.MongoMungple;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Getter
 public class UserVisitMungpleCountDTO {
+    @Schema(description = "멍플 고유 아이디")
     private Integer mungpleId;
+    @Schema(description = "방문 횟수")
     private Long visitCount;
-    private String mungplePlaceName;
-    private String mungplePhotoUrl;
+    @Schema(description = "장소 이름")
+    private String placeName;
+    @Schema(description = "사진 URL")
+    private String photoUrl;
 
     public UserVisitMungpleCountDTO(Integer mungpleId, Long visitCount){
         this.mungpleId = mungpleId;
         this.visitCount = visitCount;
     }
 
-    public UserVisitMungpleCountDTO setMungpleData(String mungplePlaceName, String mungplePhotoUrl){
-        this.mungplePlaceName = mungplePlaceName;
-        this.mungplePhotoUrl = mungplePhotoUrl;
+    public UserVisitMungpleCountDTO setMungpleData(String placeName, String photoUrl){
+        this.placeName = placeName;
+        this.photoUrl = photoUrl;
         return this;
     }
 }
