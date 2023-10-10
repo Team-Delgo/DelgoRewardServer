cdd1481103471c3038dba0f7e092a5bf5695d3a6
Gupuroom <cjsrnr1114@gmail.com>
2023-10-10 15:09:03 +0900

    ADD: AccountController Swagger 추가

--- src/main/java/com/delgo/reward/dto/user/AccountResDTO.java
+++ src/main/java/com/delgo/reward/dto/user/AccountResDTO.java
@@ -3,6 +3,7 @@ package com.delgo.reward.dto.user;
 
 import com.delgo.reward.comm.code.CategoryCode;
 import com.delgo.reward.domain.user.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.List;
@@ -13,18 +14,13 @@ import java.util.Map;
 @ToString
 @NoArgsConstructor
 public class AccountResDTO extends UserResDTO {
-    private int totalCount;
-    private int totalCountByMungple;
-
-    // Activity
+    @Schema(description = "활동 비율 표시 [Key: CategoryCode(ENUM)]")
     private Map<CategoryCode, Integer> activityMapByCategoryCode;
-    // 가장 많이 방문 한 멍플(최대 3개)
+    @Schema(description = "가장 많이 방문 한 멍플(최대 3개)")
     private List<UserVisitMungpleCountDTO> top3VisitedMungpleList;
 
-    public AccountResDTO(User user, int totalCount, int totalCountByMungple, Map<CategoryCode, Integer> activityMapByCategoryCode, List<UserVisitMungpleCountDTO> top3VisitedMungpleList) {
+    public AccountResDTO(User user, Map<CategoryCode, Integer> activityMapByCategoryCode, List<UserVisitMungpleCountDTO> top3VisitedMungpleList) {
         super(user);
-        this.totalCount = totalCount;
-        this.totalCountByMungple = totalCountByMungple;
         this.activityMapByCategoryCode = activityMapByCategoryCode;
         this.top3VisitedMungpleList = top3VisitedMungpleList;
     }
