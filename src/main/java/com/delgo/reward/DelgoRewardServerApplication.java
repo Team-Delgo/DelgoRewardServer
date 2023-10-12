a0638765a8ad38405a39196d6734fe032ca67f48
Gupuroom <cjsrnr1114@gmail.com>
2023-10-12 11:11:57 +0900

    FIX: Swagger Dev 서버에서 try it 가능하도록 수정

--- src/main/java/com/delgo/reward/DelgoRewardServerApplication.java
+++ src/main/java/com/delgo/reward/DelgoRewardServerApplication.java
@@ -1,8 +1,11 @@
 package com.delgo.reward;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://www.test.delgo.pet", description = "Dev Server")})
 @SpringBootApplication
 public class DelgoRewardServerApplication {
 
