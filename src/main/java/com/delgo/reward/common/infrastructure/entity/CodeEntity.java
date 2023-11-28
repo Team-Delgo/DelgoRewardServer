44d24d0b6b4d76121703c409d01cc6a55b9df910
Gupuroom <cjsrnr1114@naver.com>
2023-11-28 10:25:16 +0900

    FIX: CodeEntity 수정 - table 명 추가

--- src/main/java/com/delgo/reward/common/infrastructure/entity/CodeEntity.java
+++ src/main/java/com/delgo/reward/common/infrastructure/entity/CodeEntity.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "code")
 public class CodeEntity extends BaseTimeEntity {
     @Id
     private String code; // 코드
