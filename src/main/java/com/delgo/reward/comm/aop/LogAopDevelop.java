d3bf40bc59a19c8b29a0628f06bca8e91c123ada
Gupuroom <cjsrnr1114@naver.com>
2023-12-04 16:09:54 +0900

    FIX: Log 형식 수정 - 기존 Aop Log 형식 참고해서 Log 형식 수정 - MultipartFile, LocalDate의 경우 Json으로 직렬화 할 경우 에러 발생  * 에러 발생 안하게 수정

--- src/main/java/com/delgo/reward/comm/aop/LogAopDevelop.java
+++ src/main/java/com/delgo/reward/comm/aop/LogAopDevelop.java
@@ -1,6 +1,9 @@
 package com.delgo.reward.comm.aop;
 
 import com.delgo.reward.record.common.ResponseRecord;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -9,49 +12,74 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
 @Aspect
 @Component
 public class LogAopDevelop {
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping) ")
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
     private void onGetRequest() {
     }
 
-    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping) " +
-            "|| @annotation(org.springframework.web.bind.annotation.PutMapping)" +
-            "|| @annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)" +
+            "||@annotation(org.springframework.web.bind.annotation.PutMapping)" +
+            "||@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
     private void onRequest() {
     }
 
     @AfterReturning(pointcut = "onGetRequest()", returning = "responseEntity")
     public void afterReturningAdviceByGet(JoinPoint joinPoint, ResponseEntity<?> responseEntity) {
         ResponseRecord record = (ResponseRecord) responseEntity.getBody();
-        log.info("{} || Parameter : {} || Response : code = {} msg = {} || HTTP : {}", getRequestUrl(joinPoint), params(joinPoint), record.code(), record.codeMsg(), responseEntity.getStatusCode());
+        log.info("\n[LogAop]" +
+                "\n\t" + getRequestUrl(joinPoint) +
+                "\n\tparameter: " + params(joinPoint) +
+                "\n\tresponse code: " + record.code() +
+                "\n\tresponse codeMsg: " + record.codeMsg() +
+                "\n------------------------------------------------------------");
     }
 
-    @AfterThrowing(pointcut = "onGetRequest()", throwing = "ex")
-    public void afterThrowingAdviceByGet(JoinPoint joinPoint, Throwable ex) {
-        log.info("{} || Exception : {} || Parameter : {}", getRequestUrl(joinPoint), ex.getMessage(), params(joinPoint));
+    @AfterThrowing(pointcut = "onGetRequest()", throwing = "exception")
+    public void afterThrowingAdviceByGet(JoinPoint joinPoint, Throwable exception) {
+        log.info("{} || Exception : {} || Parameter : {}", getRequestUrl(joinPoint), exception.getMessage(), params(joinPoint));
+        log.error("\n[LogAop]" +
+                "\n\t" + getRequestUrl(joinPoint) +
+                "\n\tparameter: " + params(joinPoint) +
+                "\n\texception location: " + exception.getStackTrace()[0] +
+                "\n\texception msg: " + exception.getMessage() +
+                "\n------------------------------------------------------------");
     }
 
     @AfterReturning(pointcut = "onRequest()", returning = "responseEntity")
-    public void afterReturningAdvice(JoinPoint joinPoint, ResponseEntity<?> responseEntity) {
+    public void afterReturningAdvice(JoinPoint joinPoint, ResponseEntity<?> responseEntity) throws JsonProcessingException {
         ResponseRecord record = (ResponseRecord) responseEntity.getBody();
-        log.info("{} || Parameter : {} || HTTP : {}", getRequestUrl(joinPoint), params(joinPoint), responseEntity.getStatusCode());
-        log.info("Response : code = {} msg = {} data = {} ",record.code(), record.codeMsg(), record.codeMsg());
+        log.info("\n[LogAop]" +
+                "\n\t" + getRequestUrl(joinPoint) +
+                "\n\trequest body: \n " + prettyPrinter(params(joinPoint)) +
+                "\n\tresponse code: " + record.code() +
+                "\n\tresponse codeMsg: " + record.codeMsg() +
+                "\n\tresponse data: \n " + prettyPrinter(record.data()) +
+                "\n------------------------------------------------------------");
     }
 
-    @AfterThrowing(pointcut = "onRequest()", throwing = "ex")
-    public void afterThrowingAdvice(JoinPoint joinPoint, Throwable ex) {
-        log.info("{} || Exception : {} || Parameter : {}", getRequestUrl(joinPoint), ex.getMessage(), params(joinPoint));
+    @AfterThrowing(pointcut = "onRequest()", throwing = "exception")
+    public void afterThrowingAdvice(JoinPoint joinPoint, Throwable exception) throws JsonProcessingException {
+        log.error("\n[LogAop]" +
+                "\n\t" + getRequestUrl(joinPoint) +
+                "\n\trequest body: \n " + prettyPrinter(params(joinPoint)) +
+                "\n\texception location: " + exception.getStackTrace()[0] +
+                "\n\texception msg: " + exception.getMessage() +
+                "\n------------------------------------------------------------");
     }
 
     private String getRequestUrl(JoinPoint joinPoint) {
@@ -61,12 +89,10 @@ public class LogAopDevelop {
         RequestMapping requestMapping = joinPoint.getTarget().getClass().getAnnotation(RequestMapping.class);
         String classUrl = requestMapping.value()[0];
 
-        String url = Stream.of(GetMapping.class, PutMapping.class, PostMapping.class, DeleteMapping.class)
+        return Stream.of(GetMapping.class, PutMapping.class, PostMapping.class, DeleteMapping.class)
                 .filter(method::isAnnotationPresent)
                 .map(mappingClass -> getUrl(method, mappingClass, classUrl))
                 .findFirst().orElse(null);
-
-        return url;
     }
 
     /* httpMETHOD + requestURI 를 반환 */
@@ -89,8 +115,29 @@ public class LogAopDevelop {
         Object[] args = joinPoint.getArgs();
         Map<String, Object> params = new HashMap<>();
         for (int i = 0; i < parameterNames.length; i++) {
-            params.put(parameterNames[i], args[i]);
+            Object arg = args[i];
+            if (arg instanceof List<?>) {
+                List<?> list = (List<?>) arg;
+                if (!list.isEmpty() && list.get(0) instanceof MultipartFile) {
+                    List<String> fileNameList = ((List<MultipartFile>) list).stream()
+                            .map(MultipartFile::getOriginalFilename)
+                            .toList();
+                    params.put(parameterNames[i], fileNameList);
+                }
+            } else {
+                params.put(parameterNames[i], args[i]);
+            }
         }
         return params;
+    }
+
+    public String prettyPrinter(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        String requestBody = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+        return Arrays.stream(requestBody.split("\n"))
+                .map(line -> "\t\t\t" + line) // 각 줄에 탭 두 번 적용
+                .collect(Collectors.joining("\n"));
     }
 }
