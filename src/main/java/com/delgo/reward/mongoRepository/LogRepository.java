18f7e0869e13eca01c018199d2981128fa6f9e20
Gupuroom <cjsrnr1114@naver.com>
2023-12-05 15:08:25 +0900

    DEL: 사용하지 않는 Mongo Domain 삭제 - Log - MongoCert - MungpleDetail - NaverPlace

--- src/main/java/com/delgo/reward/mongoRepository/LogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.delgo.reward.mongoRepository;
-
-import com.delgo.reward.mongoDomain.Log;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogRepository extends MongoRepository<Log, String> {
-}
