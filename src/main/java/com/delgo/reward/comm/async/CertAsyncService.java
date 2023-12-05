c5b54682353145d4f36732bc51a15233b192d43e
Gupuroom <cjsrnr1114@naver.com>
2023-12-05 16:41:47 +0900

    FIX: PhotoService 리팩토링

--- src/main/java/com/delgo/reward/comm/async/CertAsyncService.java
+++ src/main/java/com/delgo/reward/comm/async/CertAsyncService.java
@@ -1,6 +1,7 @@
 package com.delgo.reward.comm.async;
 
 import com.delgo.reward.comm.ncp.greeneye.GreenEyeService;
+import com.delgo.reward.comm.ncp.storage.BucketName;
 import com.delgo.reward.domain.certification.CertPhoto;
 import com.delgo.reward.repository.CertPhotoRepository;
 import com.delgo.reward.service.CertService;
@@ -12,14 +13,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CertAsyncService {
-    @Value("${config.photoDir}")
+    @Value("${config.photo-dir}")
     String DIR;
 
     private final CertService certService;
@@ -30,27 +30,19 @@ public class CertAsyncService {
     @Async
     public void doSomething(Integer certificationId) throws JsonProcessingException {
         List<CertPhoto> certPhotos = certPhotoRepository.findPhotosByCertificationId(certificationId);
-        for(CertPhoto photo : certPhotos) {
-            String jpgUrl = photo.getUrl();
-            String[] jpgPath = jpgUrl.split("/");
-            String jpgName = jpgPath[jpgPath.length - 1]; // ex) 1359_cert_1.png
-            String fileName = jpgName.split("\\.")[0]; // ex) 1359_cert_1
-
-            File file = new File(DIR + jpgName);
+        for (CertPhoto photo : certPhotos) {
+            String fileName = photoService.getFileNameFromURL(photo.getUrl()); // ex) 1359_cert_1.png
 
             // [DIR 사진 파일 기준] 유해 사진 체크
-            boolean isCorrect = greenEyeService.checkHarmfulPhoto(jpgUrl);
+            boolean isCorrect = greenEyeService.checkHarmfulPhoto(photo.getUrl());
             photo.setIsCorrect(isCorrect);
-            if(!isCorrect){
+            if (!isCorrect) {
                 certService.changeIsCorrect(certificationId, false);
             }
 
-            String ncpLink = photoService.uploadCertPhotoWithWebp(fileName, file);
-            photo.setUrl(photoService.setCacheInvalidation(ncpLink));
-            certPhotoRepository.save(photo);
-
-            // 안 쓰는 jpg 파일 삭제
-            file.delete();
+            String uploadedURL = photoService.upload(fileName, BucketName.CERTIFICATION);
+            photo.setUrl(uploadedURL);
         }
+        certPhotoRepository.saveAll(certPhotos);
     }
 }
