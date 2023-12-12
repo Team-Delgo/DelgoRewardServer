002e0f35b4642a14d1c6660d2397d3240f05da98
Gupuroom <cjsrnr1114@naver.com>
2023-12-12 11:25:51 +0900

    FIX: Code 리팩토링 - 메서드 직관적으로 변경 - 하나의 메서드가 하나의 책임만 가지도록 수정

---
+++ src/main/java/com/delgo/reward/comm/code/CodeType.java
@@ -0,0 +1,30 @@
+package com.delgo.reward.comm.code;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(enumAsRef = true, description = """
+        CODE TYPE:
+        * `GEO` - 지역 코드
+        * `BREED` - 품종 코드
+        """)
+public enum CodeType {
+    geo("geo", "지역 코드"),
+    breed("breed", "품종 코드");
+
+    private final String value;
+    private final String desc;
+
+    CodeType(String value, String desc) {
+        this.value = value;
+        this.desc = desc;
+    }
+
+    // @RequestParam ENUM Parsing
+    @JsonCreator
+    public static CodeType from(String s) {
+        return CodeType.valueOf(s);
+    }
+}
--- src/main/java/com/delgo/reward/repository/CodeRepository.java
+++ src/main/java/com/delgo/reward/repository/CodeRepository.java
@@ -1,5 +1,6 @@
 package com.delgo.reward.repository;
 
+import com.delgo.reward.comm.code.CodeType;
 import com.delgo.reward.domain.code.Code;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -8,12 +9,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CodeRepository extends JpaRepository<Code, Integer>, JpaSpecificationExecutor<Code> {
-    List<Code> findByType(String type);
-
+    List<Code> findListByType(CodeType type);
     Optional<Code> findByCode(String code);
-
     Optional<Code> findByCodeName(String codeName);
-
     // 소문자 다음에 바로 대문자가 나올 경우 앞에 소문자로 써줘야 한다. ex findByPCode (x) findBypCode (o)
     Optional<Code> findBypCodeAndCodeName(String pCode, String codeName);
 }
