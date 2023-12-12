931f6f7a37590abd9b578f61150380f9e0da411b
Gupuroom <cjsrnr1114@naver.com>
2023-12-12 10:24:02 +0900

    FIX: ReverseGeoService 리팩토링 - GeoDataService로 GeoService랑 하나로 관리 - 기존에는 코드도 같이 반환 -> 주소만 반환하도록 수정 - 이에 따라 Certification 등록하는 로직 수정  * 추후 Certification 및 Code 리팩토링 예정

--- src/test/java/com/delgo/reward/NCPTest.java
+++ src/test/java/com/delgo/reward/NCPTest.java
@@ -1,5 +1,6 @@
 package com.delgo.reward;
 
+import com.delgo.reward.comm.ncp.geo.GeoDataService;
 import com.delgo.reward.comm.ncp.greeneye.GreenEyeService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -12,16 +13,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @Slf4j
 @SpringBootTest
-public class GreenEyeTest {
+public class NCPTest {
 
     @Autowired
     private GreenEyeService greenEyeService;
 
+    @Autowired
+    private GeoDataService geoDataService;
+
 
     @Test
-    public void check() {
+    public void greenEyeTest() {
         String url = "https://kr.object.ncloudstorage.com/test-certification/2072_cert_2.webp?2312120931044176";
         boolean result = greenEyeService.isCorrect(url);
+
+        System.out.println("result = " + result);
+    }
+
+    @Test
+    public void reverseGeoDataTest() {
+        String latitude = "37.4996921";
+        String longitude = "127.1323601";
+        String result = geoDataService.getReverseGeoData(latitude, longitude);
 
         System.out.println("result = " + result);
     }
