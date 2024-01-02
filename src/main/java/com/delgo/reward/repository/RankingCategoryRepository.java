04d1956d8ca4e54f61c0c54114d011b844f9aed4
Gupuroom <cjsrnr1114@naver.com>
2024-01-02 16:59:52 +0900

    DEL: 안 쓰는 파일 삭제

--- src/main/java/com/delgo/reward/repository/RankingCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.delgo.reward.repository;
-
-import com.delgo.reward.domain.ranking.RankingCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RankingCategoryRepository extends JpaRepository<RankingCategory, Integer> {
-    int findByUserIdAndCategoryCode(int userId, String categoryCode);
-}
