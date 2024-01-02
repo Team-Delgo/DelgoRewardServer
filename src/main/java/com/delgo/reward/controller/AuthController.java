ba78a606fd17c8976f27399bd674d39919bdb07e
Gupuroom <cjsrnr1114@naver.com>
2024-01-02 16:34:57 +0900

    FIX: AuthController 리팩토링 - 코드 컨벤션 수정 - 에러 코드 직관적으로 변경

--- src/main/java/com/delgo/reward/controller/AuthController.java
+++ src/main/java/com/delgo/reward/controller/AuthController.java
@@ -6,18 +6,16 @@ import com.delgo.reward.comm.code.APICode;
 import com.delgo.reward.dto.SmsAuthDTO;
 import com.delgo.reward.service.SmsAuthService;
 import com.delgo.reward.service.user.UserQueryService;
-import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 import java.util.Optional;
 
-@Hidden
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -27,40 +25,26 @@ public class AuthController extends CommController {
 
     // 이메일 존재 유무 확인
     @GetMapping("/email")
-    public ResponseEntity<?> emailAuth(@RequestParam String email) {
-        if (email.isBlank()) {
-            return ErrorReturn(APICode.PARAM_ERROR);
-        }
-
-        if (userQueryService.isEmailExisting(email)) {
-            return SuccessReturn(userQueryService.getOneByEmail(email).getPhoneNo());
-        }
-        return ErrorReturn(APICode.NOT_FOUND_DATA);
+    public ResponseEntity<?> emailAuth(@RequestParam @NotBlank String email) {
+        return userQueryService.isEmailExisting(email)
+                ? SuccessReturn(userQueryService.getOneByEmail(email).getPhoneNo())
+                : ErrorReturn(APICode.NOT_FOUND_DATA);
     }
 
     // 이메일 중복 확인
     @GetMapping("/email/check")
-    public ResponseEntity<?> emailCheck(@RequestParam String email) {
-        if (email.isBlank()) {
-            return ErrorReturn(APICode.PARAM_ERROR);
-        }
-        if (!userQueryService.isEmailExisting(email)) {
-            return SuccessReturn();
-        } else {
-            return ErrorReturn(APICode.SERVER_ERROR);
-        }
+    public ResponseEntity<?> emailCheck(@RequestParam @NotBlank String email) {
+        return userQueryService.isEmailExisting(email)
+                ? ErrorReturn(APICode.EMAIL_DUPLICATE_ERROR)
+                : SuccessReturn();
     }
 
     // 이름 중복 확인
     @GetMapping("/name/check")
-    public ResponseEntity<?> nameCheck(@RequestParam String name) {
-        if (name.isBlank()) {
-            return ErrorReturn(APICode.PARAM_ERROR);
-        }
-        if (!userQueryService.isNameExisting(name))
-            return SuccessReturn();
-        else
-            return ErrorReturn(APICode.SERVER_ERROR);
+    public ResponseEntity<?> nameCheck(@RequestParam @NotBlank String name) {
+        return userQueryService.isNameExisting(name)
+                ? ErrorReturn(APICode.NAME_DUPLICATE_ERROR)
+                : SuccessReturn();
     }
 
     // 인증번호 생성
@@ -76,17 +60,13 @@ public class AuthController extends CommController {
 
     // 인증번호 확인
     @GetMapping("/sms/check")
-        public ResponseEntity<?> randNumCheck(@RequestParam Integer smsId, @RequestParam String enterNum) {
+        public ResponseEntity<?> randNumCheck(@RequestParam @NotNull Integer smsId, @RequestParam @NotBlank String enterNum) {
             if(Objects.equals(enterNum, "9999")) // 9999로 무조건 되는 코드 추가
                 return SuccessReturn();
 
-        if (enterNum.isBlank()) {
-            return ErrorReturn(APICode.PARAM_ERROR);
-        }
         Optional<APICode> apiCode = smsAuthService.checkSMS(smsId, enterNum);
-        if (apiCode.isPresent())
-            return ErrorReturn(apiCode.get());
-
-        return SuccessReturn();
+        return  smsAuthService.checkSMS(smsId, enterNum).isPresent()
+                ? ErrorReturn(apiCode.get())
+                : SuccessReturn();
     }
 }
