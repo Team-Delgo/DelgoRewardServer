9f64c3b348b5861a9632b259726cb3376fbfedc0
Gupuroom <cjsrnr1114@naver.com>
2024-01-03 10:50:45 +0900

    FIX: Comment Request, Response 명명 변경 ResDTO -> Response Record -> Create Or Update

--- src/main/java/com/delgo/reward/comment/controller/request/CommentCreate.java
+++ src/main/java/com/delgo/reward/comment/controller/request/CommentCreate.java
@@ -2,24 +2,26 @@ package com.delgo.reward.comment.controller.request;
 
 import com.delgo.reward.comment.domain.Comment;
 import com.delgo.reward.user.domain.User;
+import lombok.Builder;
 
 import javax.validation.constraints.NotNull;
 
-public record ReplyRecord(
+
+@Builder
+public record CommentCreate(
         @NotNull Integer userId,
         @NotNull Integer certificationId,
-        @NotNull Integer parentCommentId,
         @NotNull String content
 ) {
     public Comment toEntity(User user) {
         return Comment.builder()
                 .user(user)
                 .certificationId(certificationId)
-                .parentCommentId(this.parentCommentId())
                 .content(this.content())
-                .isReply(true)
+                .isReply(false)
                 .build();
     }
 }
+
 
 
