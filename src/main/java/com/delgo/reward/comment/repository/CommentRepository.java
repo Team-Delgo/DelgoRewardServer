58956e15b5136e563b1ff90f2a6579f210a0c6c5
Gupuroom <cjsrnr1114@naver.com>
2024-01-03 11:38:01 +0900

    FIX: Comment 리팩토링, Service, Repository 메서드 명 변경

--- src/main/java/com/delgo/reward/comment/repository/CommentRepository.java
+++ src/main/java/com/delgo/reward/comment/repository/CommentRepository.java
@@ -10,14 +10,14 @@ import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer>{
     @Query(value = "select c from Comment c join fetch c.user u join fetch u.pet where c.certificationId = :certId")
-    List<Comment> findCommentsByCertId(@Param("certId") int certId);
+    List<Comment> findListByCertId(@Param("certId") int certId);
 
     @Query(value = "select c from Comment c join fetch c.user u join fetch u.pet where c.commentId = :commentId")
-    Optional<Comment> findCommentsById(@Param("commentId") int commentId);
+    Optional<Comment> findOneByCommentId(@Param("commentId") int commentId);
 
     @Query(value = "select count(*) from Comment c where c.certificationId = :certId")
-    Integer countCommentByCertId(@Param("certId") int certId);
+    Integer countByCertId(@Param("certId") int certId);
 
     @Query(value = "select c from Comment c where c.parentCommentId = :parentCommentId and c.isReply = true")
-    List<Comment> findCommentsByParentCommentId(@Param("parentCommentId") int parentCommentId);
+    List<Comment> findListByParentCommentId(@Param("parentCommentId") int parentCommentId);
 }
--- src/main/java/com/delgo/reward/comment/service/CommentService.java
+++ src/main/java/com/delgo/reward/comment/service/CommentService.java
@@ -14,14 +14,12 @@ import com.delgo.reward.comment.repository.CommentRepository;
 import com.delgo.reward.cert.service.CertQueryService;
 import com.delgo.reward.user.service.UserQueryService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
 
-@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -37,29 +35,15 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     /**
-     * 댓글에 대한 알림 내용
-     */
-    public String makeCommentNotifyMsg(String name, String content){
-        return name + "님이 나의 게시글에 댓글을 남겼습니다.\n" + content;
-    }
-
-    /**
-     * 답글에 대한 알림 내용
-     */
-    public String makeReplyNotifyMsg(String name, String content){
-        return name + "님이 나의 댓글에 답글을 남겼습니다.\n" + content;
-    }
-
-    /**
      *  유저가 댓글을 작성하면 알림을 저장하고 인증 주인에게 푸시 알림을 보냄
      */
-    public Comment createComment(CommentCreate commentCreate) throws IOException {
+    public Comment create(CommentCreate commentCreate) throws IOException {
         User user = userQueryService.getOneByUserId(commentCreate.userId()); // 댓글 작성 유저 조회
         Comment comment = commentRepository.save(Comment.from(commentCreate, user)); // 댓글 저장
-        Certification certification = certQueryService.getOneById(commentCreate.certificationId()); // 댓글 저장한 인증글 조회
 
         // commentCount 계산
-        int commentCount = commentRepository.countCommentByCertId(commentCreate.certificationId());
+        Certification certification = certQueryService.getOneById(commentCreate.certificationId()); // 댓글 저장한 인증글 조회
+        int commentCount = commentRepository.countByCertId(commentCreate.certificationId());
         certification.setCommentCount(commentCount);
 
         // 인증 유저 알림
@@ -74,36 +58,36 @@ public class CommentService {
     /**
      * [commentId] 뎃글 조회
      */
-    public Comment getCommentById(int commentId){
-        return commentRepository.findCommentsById(commentId)
+    public Comment getOneById(int commentId){
+        return commentRepository.findOneByCommentId(commentId)
                 .orElseThrow(() -> new NotFoundDataException("[Comment] commentId : " + commentId));
     }
 
     /**
      * [certificationId] 뎃글 조회
      */
-    public List<Comment> getCommentsByCertId(int certificationId){
-        return commentRepository.findCommentsByCertId(certificationId);
+    public List<Comment> getListByCertId(int certificationId){
+        return commentRepository.findListByCertId(certificationId);
     }
 
     /**
      * 댓글 수정
      */
-    public Boolean modifyComment(CommentUpdate modifyCommentRecord) {
-        Comment comment = getCommentById(modifyCommentRecord.commentId());
-        if (comment.getUser().getUserId() != modifyCommentRecord.userId())  // 유저 체크
+    public Boolean update(CommentUpdate commentUpdate) {
+        Comment comment = getOneById(commentUpdate.commentId());
+        if (comment.getUser().getUserId() != commentUpdate.userId())  // 유저 체크
             return false;
 
-        comment.setContent(modifyCommentRecord.content());
+        comment.setContent(commentUpdate.content());
         return true;
     }
 
     /**
      * 댓글 삭제
      */
-    public Boolean deleteComment(int commentId, int userId, int certificationId) {
+    public Boolean delete(int commentId, int userId, int certificationId) {
         // 댓글 OR 인증 작성자인지 CHECK.
-        Comment comment = getCommentById(commentId);
+        Comment comment = getOneById(commentId);
         Certification certification = certQueryService.getOneById(certificationId);
         if (comment.getUser().getUserId() != userId && certification.getUser().getUserId() != userId)  // 유저 체크
             return false;
@@ -112,7 +96,7 @@ public class CommentService {
         commentRepository.deleteById(commentId);
 
         // commentCount 업데이트
-        int commentCount = commentRepository.countCommentByCertId(certificationId);
+        int commentCount = commentRepository.countByCertId(certificationId);
         certification.setCommentCount(commentCount);
 
         return true;
@@ -130,7 +114,7 @@ public class CommentService {
         Certification certification = certQueryService.getOneById(replyCreate.certificationId()); // 답글 저장한 인증글 조회
 
         // commentCount 계산
-        int commentCount = commentRepository.countCommentByCertId(replyCreate.certificationId());
+        int commentCount = commentRepository.countByCertId(replyCreate.certificationId());
         certification.setCommentCount(commentCount);
 
         // 인증 유저 알림
@@ -140,7 +124,7 @@ public class CommentService {
         fcmService.commentPush(certOwnerId, certOwnerNotifyMsg);
 
         // 부모 댓글 유저 알림
-        int commentOwnerId = getCommentById(replyCreate.parentCommentId()).getUser().getUserId();
+        int commentOwnerId = getOneById(replyCreate.parentCommentId()).getUser().getUserId();
         String commentOwnerNotifyMsg = makeReplyNotifyMsg(user.getName(), replyCreate.content());
         notifyService.saveNotify(commentOwnerId, NotifyType.REPLY, commentOwnerNotifyMsg);
         fcmService.commentPush(commentOwnerId, commentOwnerNotifyMsg);
@@ -151,7 +135,15 @@ public class CommentService {
     /**
      * [parentCommentId] 댓글에 대한 답글 조회
      */
-    public List<Comment> getReplyByParentCommentId(int parentCommentId){
-        return commentRepository.findCommentsByParentCommentId(parentCommentId);
+    public List<Comment> getListByParentCommentId(int parentCommentId){
+        return commentRepository.findListByParentCommentId(parentCommentId);
+    }
+
+    public String makeCommentNotifyMsg(String name, String content){
+        return name + "님이 나의 게시글에 댓글을 남겼습니다.\n" + content;
+    }
+
+    public String makeReplyNotifyMsg(String name, String content){
+        return name + "님이 나의 댓글에 답글을 남겼습니다.\n" + content;
     }
 }
