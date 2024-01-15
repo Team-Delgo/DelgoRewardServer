d1727c38ec86fdc4482f35f6de2566e91a8fcf94
Gupuroom <cjsrnr1114@naver.com>
2024-01-15 10:22:58 +0900

    ADD: PushController 추가 (운영진 직접 푸쉬 가능하게 API 추가)
    - Push 관련 soutv 코드 삭제
    - IOS mutableContent 추가
    
    - version 1.1.8로 변경

---
+++ src/main/java/com/delgo/reward/push/controller/PushController.java
@@ -0,0 +1,51 @@
+package com.delgo.reward.push.controller;
+
+import com.delgo.reward.cert.domain.Certification;
+import com.delgo.reward.cert.service.CertQueryService;
+import com.delgo.reward.common.controller.CommController;
+import com.delgo.reward.mungple.domain.Mungple;
+import com.delgo.reward.mungple.service.MungpleService;
+import com.delgo.reward.push.service.FcmService;
+import com.delgo.reward.user.service.UserQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/push")
+public class PushController extends CommController {
+    private final FcmService fcmService;
+    private final MungpleService mungpleService;
+    private final CertQueryService certQueryService;
+    private final UserQueryService userQueryService;
+
+    @GetMapping("/mungple")
+    public ResponseEntity createTokenFromAndroid(@RequestParam int mungpleId, @RequestParam boolean isUserFound) {
+        Mungple mungple = mungpleService.getOneByMungpleId(mungpleId);
+
+        if (isUserFound) {
+            // 멍플 이전 등록한 User
+            List<Integer> founderIdList = certQueryService.getListByPlaceName(mungple.getPlaceName()).stream()
+                    .map(Certification::getUserId)
+                    .peek(founderId -> fcmService.mungpleByMe(founderId, mungple.getMungpleId()))
+                    .toList();
+
+            // 그 외 ( 지역으로 조회)
+            userQueryService.getListByPGeoCode(mungple.getPGeoCode()).stream()
+                    .filter(user -> !founderIdList.contains(user.getUserId()))
+                    .forEach(receiver -> fcmService.mungpleByOther(founderIdList.get(0), receiver.getUserId(),
+                            mungple.getMungpleId()));
+        } else {
+            userQueryService.getListByPGeoCode(mungple.getPGeoCode())
+                    .forEach(user -> fcmService.mungple(user.getUserId(), mungple.getMungpleId()));
+        }
+        return SuccessReturn();
+    }
+}
