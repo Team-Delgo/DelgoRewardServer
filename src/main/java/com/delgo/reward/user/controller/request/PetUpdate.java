c4b9172fd01ea1987d2e1cf4511c53be8eca11f5
Gupuroom <cjsrnr1114@naver.com>
2024-01-18 15:15:42 +0900

    test: Pet (Domain, Service) 1. update 비즈니스 로직 Repository -> Domain 이동  * 테스트 용이성 확보 2. getOneByUserId 추가  * delete 검증 위해 필요

--- src/main/java/com/delgo/reward/user/controller/request/PetUpdate.java
+++ src/main/java/com/delgo/reward/user/controller/request/PetUpdate.java
@@ -1,8 +1,11 @@
 package com.delgo.reward.user.controller.request;
 
+import lombok.Builder;
+
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@Builder
 public record PetUpdate(@NotNull String email,
                         String name,
                         LocalDate birthday,
--- src/main/java/com/delgo/reward/user/service/PetService.java
+++ src/main/java/com/delgo/reward/user/service/PetService.java
@@ -3,15 +3,13 @@ package com.delgo.reward.user.service;
 
 import com.delgo.reward.code.domain.Code;
 import com.delgo.reward.code.service.CodeService;
+import com.delgo.reward.comm.exception.NotFoundDataException;
 import com.delgo.reward.user.domain.Pet;
-import com.delgo.reward.user.domain.User;
 import com.delgo.reward.user.controller.request.PetUpdate;
 import com.delgo.reward.user.repository.PetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,26 +22,25 @@ public class PetService {
     public Pet create(Pet pet) {
         Code code = codeService.getOneByCode(pet.getBreed());
         pet.setBreedName(code.getCodeName());
-        Pet savedPet = petRepository.save(pet);
 
-        User user = userQueryService.getOneByUserId(pet.getUserId());
-        user.setPet(savedPet);
-
-        return savedPet;
+        return petRepository.save(pet);
     }
 
     @Transactional
-    public void changePetInfo(PetUpdate petUpdate, User user){
-        Optional.ofNullable(petUpdate.name()).ifPresent(user.getPet()::setName);
-        Optional.ofNullable(petUpdate.birthday()).ifPresent(user.getPet()::setBirthday);
-        Optional.ofNullable(petUpdate.breed()).ifPresent(breed ->{
-            user.getPet().setBreed(breed);
-            user.getPet().setBreedName(codeService.getOneByCode(breed).getCodeName());
-        });
+    public Pet update(PetUpdate petUpdate) {
+        Pet pet = userQueryService.getOneByEmail(petUpdate.email()).getPet();
+        Code breedCode = codeService.getOneByCode(petUpdate.breed());
+
+        return petRepository.save(pet.update(petUpdate, breedCode.getCodeName()));
     }
 
     @Transactional
     public void deleteByUserId(int userId){
         petRepository.deleteByUserUserId(userId);
+    }
+
+    public Pet getOneByUserId(int userId){
+        return petRepository.findByUserUserId(userId)
+                .orElseThrow(() -> new NotFoundDataException("[Pet] userId : " + userId));
     }
 }
